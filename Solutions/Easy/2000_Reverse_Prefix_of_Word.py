6d698322f8385589e857fc4d871c44bae734ec97
Sharvil21 <sshastri2102@gmail.com>
2024-08-10 18:00:40 -0400

    Added the Second Solution

--- Solutions/Easy/2000_Reverse_Prefix_of_Word.py
+++ Solutions/Easy/2000_Reverse_Prefix_of_Word.py
@@ -5,3 +5,7 @@ class Solution:
             return word[:word.index(ch)+1][::-1] + word[word.index(ch)+1:]
         else:
             return word
+#second Solution:
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        return word[:word.index(ch)+1][::-1] + word[word.index(ch)+1:] if ch in word else word
