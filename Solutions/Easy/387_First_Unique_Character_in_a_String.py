cdf21a4925f2f309f9eba273a31dc97b54273b95
Sharvil21 <sshastri2102@gmail.com>
2024-08-21 15:04:04 -0400

    Added the Correct Solution

--- Solutions/Easy/387_First_Unique_Character_in_a_String.py
+++ Solutions/Easy/387_First_Unique_Character_in_a_String.py
@@ -1,1 +1,9 @@
-
+#First Solution: Using Counter() Function
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+        for i in s:
+            if count[i] == 1:
+                return s.index(i)
+        return -1
+                
