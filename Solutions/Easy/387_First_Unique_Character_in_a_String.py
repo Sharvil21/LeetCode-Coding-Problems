79fb5117693e0c634c78298cd7fa04ecaec5bd01
Sharvil21 <sshastri2102@gmail.com>
2024-08-21 15:11:59 -0400

    Added the second solution

--- Solutions/Easy/387_First_Unique_Character_in_a_String.py
+++ Solutions/Easy/387_First_Unique_Character_in_a_String.py
@@ -6,4 +6,20 @@ class Solution:
             if count[i] == 1:
                 return s.index(i)
         return -1
-                
+
+#Second Solution: Create a empty dictionary. Add the count of each character. Iterate through the dictionary and return the first character that has count = 1
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        #2nd solution using dictionary
+        empty_dict = {}
+        for i in s:
+            if i in empty_dict:
+                empty_dict[i] += 1
+            else:
+                empty_dict[i] = 1
+        
+        for i in s:
+            if empty_dict[i] == 1:
+                return s.index(i)
+        return -1
+                                
