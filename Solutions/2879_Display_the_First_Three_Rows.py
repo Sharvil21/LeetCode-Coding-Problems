08215e62aa414fcca17ab7ebbf421dff8865cf7b
Sharvil21 <sshastri2102@gmail.com>
2024-08-24 13:18:06 -0400

    Added the 2nd solution

--- Solutions/2879_Display_the_First_Three_Rows.py
+++ Solutions/2879_Display_the_First_Three_Rows.py
@@ -1,4 +1,11 @@
+#First solution
 import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     return employees.head(3)
+
+#Second Solution
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees[0:3]
