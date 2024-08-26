1e7374352f0ae7453369c4d0933ce29dbc9fedaf
Sharvil21 <sshastri2102@gmail.com>
2024-08-26 14:32:36 -0400

    Added the first solution to file

---
+++ Solutions/Easy/242_Valid_Anagram.py
@@ -0,0 +1,5 @@
+#first solution using collections.Counter
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
