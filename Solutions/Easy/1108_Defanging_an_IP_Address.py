42fc4a064370febc0cd8cafd791b8c8876e2e8f6
Sharvil21 <sshastri2102@gmail.com>
2024-08-26 15:49:55 -0400

    Added the Second solution

--- Solutions/Easy/1108_Defanging_an_IP_Address.py
+++ Solutions/Easy/1108_Defanging_an_IP_Address.py
@@ -2,3 +2,15 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return address.replace('.','[.]')
+
+#Second Solution
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        ans=""
+        for i in address:
+            if i!=".":
+                ans+=i
+            else:
+                ans+="[.]"
+        return ans
+                
