1d91468bf2457b87121d36b5d8dff4cbdc7ef355
Sharvil21 <sshastri2102@gmail.com>
2024-08-27 14:35:43 -0400

    Made modifications

--- Solutions/Easy/242_Valid_Anagram.py
+++ Solutions/Easy/242_Valid_Anagram.py
@@ -4,5 +4,9 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 	return Counter(s) == Counter(t)
 
-#2nd solution:
+#2nd solution(Using sorted() function. But this increases time complexity)
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
 
