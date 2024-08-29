0562f7eca3fbd7d4ec4e457dd99eae372913f29a
Sharvil21 <sshastri2102@gmail.com>
2024-08-29 16:44:10 -0400

    Added the Solution

--- Solutions/Easy/3280_Convert_Date_to_Binary.py
+++ Solutions/Easy/3280_Convert_Date_to_Binary.py
@@ -1,1 +1,9 @@
 #
+class Solution:
+    def convertDateToBinary(self, date: str) -> str:
+        binary_notation = ''
+        for i in date.split('-'):
+            bin_number = bin(int(i))
+            binary_notation += bin_number[2:] + '-'
+
+        return binary_notation[:-1]
