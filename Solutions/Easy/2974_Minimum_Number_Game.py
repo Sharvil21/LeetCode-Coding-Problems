08b7d31c94f547088be12c3ca3f3c064d8be027f
Sharvil21 <sshastri2102@gmail.com>
2024-08-30 20:24:15 -0400

    Added the solution

---
+++ Solutions/Easy/2974_Minimum_Number_Game.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        first_min = 0
+        second_min = 0
+        output = []
+        while len(nums) != 0:
+            for i in range(len(nums)):
+                first_min = min(nums)
+                nums.pop(nums.index(min(nums)))
+                break
+
+            for j in range(len(nums)):
+                second_min = min(nums)
+                nums.pop(nums.index(min(nums)))
+                break
+                
+            output.append(second_min)
+            output.append(first_min)
+            
+        
+        return output
+                    
