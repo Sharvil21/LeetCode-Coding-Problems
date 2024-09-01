4230ad26d52c53534719c1c967cd56a2c2216522
Sharvil21 <sshastri2102@gmail.com>
2024-08-31 20:57:09 -0400

    Added the second solution

--- Solutions/Easy/2885_Rename_Columns.py
+++ Solutions/Easy/2885_Rename_Columns.py
@@ -6,3 +6,8 @@ def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
     return students
 
 #Second Solution
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(columns={'id':'student_id','first':'first_name','last':'last_name','age':'age_in_years'}, inplace=True)
+    return students
