5a32a8c16c4fca0073fb083eb0b25da9378dcfd2
Sharvil21 <sshastri2102@gmail.com>
2024-08-31 21:08:25 -0400

    Added the second solutions

--- Solutions/Easy/2894_Divisible_and_Non-divisible_Sums_Difference.py
+++ Solutions/Easy/2894_Divisible_and_Non-divisible_Sums_Difference.py
@@ -10,4 +10,15 @@ class Solution:
                 num2.append(i)
         return sum(num1) - sum(num2)
 
-#
+#Second Solution
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = 0
+        num2 = 0
+        for i in range(1,n+1):
+            if i%m != 0:
+                num1 += i
+            elif i%m == 0 :
+                num2 += i
+        
+        return num1-num2
