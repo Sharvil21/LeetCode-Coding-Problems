e25b6a37b260c62b6dccd77c0f6e3db9581d9bfd
Sharvil21 <sshastri2102@gmail.com>
2024-09-01 15:51:37 -0400

    Added the first solution

--- Solutions/Easy/586_Customer_Placing_the_Largest_Number_of_Orders.sql
+++ Solutions/Easy/586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -1,1 +1,7 @@
-
+#First solution using CTE
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT customer_number, COUNT(order_number) AS total_orders
+FROM Orders
+GROUP BY 1
+ORDER BY 2 DESC)
+SELECT customer_number FROM cte LIMIT 1
