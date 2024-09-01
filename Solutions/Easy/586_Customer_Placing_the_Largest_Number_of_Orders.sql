2133a08a3aa33a3c45fa217e017c5db71cb9001a
Sharvil21 <sshastri2102@gmail.com>
2024-09-01 15:53:45 -0400

    Added the second solution. This one beats 76.94% so improvement in efficiency

--- Solutions/Easy/586_Customer_Placing_the_Largest_Number_of_Orders.sql
+++ Solutions/Easy/586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -5,3 +5,12 @@ FROM Orders
 GROUP BY 1
 ORDER BY 2 DESC)
 SELECT customer_number FROM cte LIMIT 1
+
+#Second solution using CTE & Dense Rank Window Function
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+customer_number, DENSE_RANK() OVER(ORDER BY COUNT(order_number) DESC) as rnk
+FROM Orders
+GROUP BY 1)
+
+SELECT customer_number FROM cte WHERE rnk = 1;
