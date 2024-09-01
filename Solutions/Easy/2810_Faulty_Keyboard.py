9de274be585b0041b574cc61cc16590e241e87f4
Sharvil21 <sshastri2102@gmail.com>
2024-09-01 15:47:19 -0400

    Added the first solution

--- Solutions/Easy/2810_Faulty_Keyboard.py
+++ Solutions/Easy/2810_Faulty_Keyboard.py
@@ -1,1 +1,10 @@
 #First solution: Beats around 38%
+class Solution:
+    def finalString(self, s: str) -> str:
+        word = []
+        for i in s:
+            if i != 'i':
+                word.append(i)
+            else:
+                word = word[::-1]
+        return ''.join(word)
