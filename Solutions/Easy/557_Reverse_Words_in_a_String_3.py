5c0a9ff35dcba5c5c1f595d3d7b96968b285d3ae
Sharvil21 <sshastri2102@gmail.com>
2024-09-02 17:00:57 -0400

    Added the first solution

--- Solutions/Easy/557_Reverse_Words_in_a_String_3.py
+++ Solutions/Easy/557_Reverse_Words_in_a_String_3.py
@@ -1,1 +1,7 @@
 #First Solution
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        output = ''
+        for i in s.split():
+            output = output + i[::-1] + ' '
+        return output.strip()
