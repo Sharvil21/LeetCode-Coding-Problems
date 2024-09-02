d7b8cb328f38d842374222edad01a370f67151d7
Sharvil21 <sshastri2102@gmail.com>
2024-09-02 16:10:54 -0400

    Added the second solution

--- Solutions/Easy/586_Customer_Placing_the_Largest_Number_of_Orders.py
+++ Solutions/Easy/586_Customer_Placing_the_Largest_Number_of_Orders.py
@@ -4,4 +4,9 @@ import pandas as pd
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
     return orders.groupby('customer_number')['order_number'].count().sort_values(ascending=False).head(1).reset_index()[['customer_number']]
 
-#Second Solution
+#Second Solution Using .mode() method:
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    return orders['customer_number'].mode().to_frame()
+
