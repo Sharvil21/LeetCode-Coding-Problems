87617e4bde9e5dc8d07f243598105d4581c2fe31
Sharvil21 <sshastri2102@gmail.com>
2024-09-04 17:22:18 -0400

    Added the Solution

--- Solutions/Easy/1587_Bank_Account_Summary_2.py
+++ Solutions/Easy/1587_Bank_Account_Summary_2.py
@@ -1,1 +1,9 @@
 #Solution using pandas .query() method as well as merge() and groupby()
+import pandas as pd
+
+def account_summary(users: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    return (transactions
+            .groupby('account')[['amount']].sum()
+            .join(users.set_index('account'))
+            .query('amount > 10000')
+            [['name', 'amount']].rename(columns={'amount': 'balance'}))
