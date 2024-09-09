1bf8babea5814b69ca0cde20d6046750026dc4e1
Sharvil21 <sshastri2102@gmail.com>
2024-09-08 20:40:30 -0400

    Added the second solution

--- Solutions/Easy/595_Big_Countries.py
+++ Solutions/Easy/595_Big_Countries.py
@@ -6,3 +6,8 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     return big_country[['name','population','area']]
 
 #Second Solution
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    return df[['name', 'population', 'area']]
