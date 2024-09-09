261bf3b8588962d7cdec5f12ea26f75650ca849b
Sharvil21 <sshastri2102@gmail.com>
2024-09-08 20:39:26 -0400

    Made modifications for second solution

--- Solutions/Easy/595_Big_Countries.py
+++ Solutions/Easy/595_Big_Countries.py
@@ -4,3 +4,5 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     big_country = world.query("area >= 3000000 or population >= 25000000")
     return big_country[['name','population','area']]
+
+#
