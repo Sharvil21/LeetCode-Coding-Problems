29c6a6ec88539d66b1eecda7ba70e6a7464a8a44
Sharvil21 <sshastri2102@gmail.com>
2024-09-09 13:04:36 -0400

    Added the first solution

--- Solutions/Easy/1757_Recyclable_and_Low_Fat_Products.py
+++ Solutions/Easy/1757_Recyclable_and_Low_Fat_Products.py
@@ -1,1 +1,5 @@
 #First Solution
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products.query("low_fats == 'Y' and recyclable == 'Y'")['product_id'].to_frame()
