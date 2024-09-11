07ec38aca4c14d54c63f61fa37a3f251f1524f43
Sharvil21 <sshastri2102@gmail.com>
2024-09-11 13:40:22 -0400

    Adding the solution

--- Solutions/Easy/183_Customers_Who_Never_Order.py
+++ Solutions/Easy/183_Customers_Who_Never_Order.py
@@ -1,1 +1,6 @@
 #Solution:
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = customers.merge(orders,left_on='id',right_on='customerId',how='left')
+    return df.query('id_y.isna()')['name'].to_frame().rename(columns={'name':'Customers'})
