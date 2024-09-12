2b803feb3bfb5828fb8918bc938a374992e2280c
Sharvil21 <sshastri2102@gmail.com>
2024-09-12 13:55:46 -0400

    Added the PostgreSQL Solution

--- Solutions/Easy/1148_Article_Views_1.sql
+++ Solutions/Easy/1148_Article_Views_1.sql
@@ -7,3 +7,8 @@ WHERE viewer_id = author_id
 ORDER BY 1 ASC;
 
 --PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+SELECT
+DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id;
