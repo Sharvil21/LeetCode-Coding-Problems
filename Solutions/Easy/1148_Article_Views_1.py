eca8fb73ee7cef086a77b09648e8065c87d299cd
Sharvil21 <sshastri2102@gmail.com>
2024-09-13 14:01:37 -0400

    Added the solution

--- Solutions/Easy/1148_Article_Views_1.py
+++ Solutions/Easy/1148_Article_Views_1.py
@@ -1,1 +1,5 @@
 #Solution
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+     return views.query("author_id == viewer_id")['author_id'].sort_values().drop_duplicates().to_frame().rename(columns={'author_id':'id'})
