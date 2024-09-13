930d7489a8d27cec6a87d16b8ed414c68bbccbe6
Sharvil21 <sshastri2102@gmail.com>
2024-09-13 16:36:43 -0400

    Added the MySQL Solution

--- Solutions/Easy/1873_Calculate_Special_Bonus.sql
+++ Solutions/Easy/1873_Calculate_Special_Bonus.sql
@@ -1,1 +1,6 @@
 --MySQL Solution
+# Write your MySQL query statement below
+SELECT
+employee_id, CASE WHEN employee_id%2!=0 AND SUBSTRING(name,1,1)!='M' THEN salary ELSE 0 END AS bonus
+FROM Employees
+ORDER BY 1;
