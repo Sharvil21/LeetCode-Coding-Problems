8b6f645edc4e421053ddf0d6be76b51bcb5d397e
Sharvil21 <sshastri2102@gmail.com>
2024-09-14 14:17:53 -0400

    Added the second MySQL Solution

--- Solutions/Easy/1683_Invalid_Tweets.sql
+++ Solutions/Easy/1683_Invalid_Tweets.sql
@@ -6,3 +6,8 @@ FROM Tweets
 WHERE LENGTH(content) > 15
 
 -- MySQL Solution with CHAR_LENGTH
+# Write your MySQL query statement below
+SELECT
+tweet_id
+FROM Tweets
+WHERE CHAR_LENGTH(content) > 15
