6b52f8086b442086c5142e514a04225c88c631fe
Sharvil21 <sshastri2102@gmail.com>
2024-09-14 14:14:28 -0400

    Added the solution

--- Solutions/Easy/1683_Invalid_Tweets.py
+++ Solutions/Easy/1683_Invalid_Tweets.py
@@ -1,1 +1,6 @@
 #Solution
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    tweets['tweet_length'] = tweets['content'].str.len()
+    return tweets.query("tweet_length > 15")['tweet_id'].to_frame()
