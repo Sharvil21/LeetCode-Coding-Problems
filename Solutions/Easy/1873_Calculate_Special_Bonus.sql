6eef4e07984a60fe13547252b2f69d23bca33a71
Sharvil21 <sshastri2102@gmail.com>
2024-09-15 16:38:15 -0400

    Added the PostgreSQL Solution

--- Solutions/Easy/1873_Calculate_Special_Bonus.sql
+++ Solutions/Easy/1873_Calculate_Special_Bonus.sql
@@ -1,8 +1,11 @@
 --MySQL Solution
-# Write your MySQL query statement below
 SELECT
 employee_id, CASE WHEN employee_id%2!=0 AND SUBSTRING(name,1,1)!='M' THEN salary ELSE 0 END AS bonus
 FROM Employees
 ORDER BY 1;
 
 --PostgreSQL Solution
+SELECT
+employee_id, CASE WHEN employee_id%2!=0 AND SUBSTRING(name,1,1)!='M' THEN salary ELSE 0 END AS bonus
+FROM Employees
+ORDER BY 1;
