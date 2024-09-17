ce507de9ef351af4210295fc9f75701a59c451ea
Sharvil21 <sshastri2102@gmail.com>
2024-09-17 15:06:55 -0400

    Added the first PostgreSQL Solution

--- Solutions/Easy/176_Second_Highest_Salary_PostgreSQL_Solution.sql
+++ Solutions/Easy/176_Second_Highest_Salary_PostgreSQL_Solution.sql
@@ -1,1 +1,11 @@
 --PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+DISTINCT salary, DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+FROM Employee)
+
+SELECT
+(SELECT salary
+FROM cte
+WHERE rnk = 2)
+AS SecondHighestSalary
