5db4add367060f2e1a215ff1f640f98317e19006
Sharvil21 <sshastri2102@gmail.com>
2024-09-17 15:07:59 -0400

    Added the second PostgreSQL Solution

--- Solutions/Easy/176_Second_Highest_Salary_PostgreSQL_Solution.sql
+++ Solutions/Easy/176_Second_Highest_Salary_PostgreSQL_Solution.sql
@@ -10,4 +10,15 @@ FROM cte
 WHERE rnk = 2)
 AS SecondHighestSalary
 
---Second Postgresql Solutio
+--Second Postgresql Solution
+-- Write your PostgreSQL query statement below
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+DISTINCT salary, DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+FROM Employee)
+
+SELECT salary AS SecondHighestSalary FROM cte WHERE rnk = 2
+UNION
+SELECT NULL
+LIMIT 1;
+
