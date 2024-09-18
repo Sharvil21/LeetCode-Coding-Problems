5260aa90c0dfdaa1a54d48e57bd25f7ef523826b
Sharvil21 <sshastri2102@gmail.com>
2024-09-18 15:21:59 -0400

    Added the PostgreSQL Solution

--- Solutions/Easy/184_Department_Highest_Salary_PostgreSQL_Solution.sql
+++ Solutions/Easy/184_Department_Highest_Salary_PostgreSQL_Solution.sql
@@ -1,1 +1,11 @@
 --PostgreSQL Solution
+WITH cte AS (
+    SELECT
+    d.name AS Department, e.name AS Employee, e.salary AS Salary, DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+)
+SELECT
+Department, Employee, Salary
+FROM cte 
+WHERE rnk = 1
