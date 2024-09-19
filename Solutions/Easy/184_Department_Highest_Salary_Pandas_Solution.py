d760fd5b98d44b7a028470b8afdc6eb4f3d204fc
Sharvil21 <sshastri2102@gmail.com>
2024-09-19 15:29:38 -0400

    Added the Pandas Solution

--- Solutions/Easy/184_Department_Highest_Salary_Pandas_Solution.py
+++ Solutions/Easy/184_Department_Highest_Salary_Pandas_Solution.py
@@ -4,3 +4,10 @@ import pandas as pd
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
     df = employee.merge(department, left_on='departmentId',right_on='id',how='inner')
     df['rnk'] = df.groupby('name_y')['salary'].rank(method='dense',ascending=False)
+    return df.query('rnk == 1')[['name_y','name_x','salary']].rename(columns={'name_y':'Department','name_x':'Employee','salary':'Salary'})
+
+
+
+
+
+
