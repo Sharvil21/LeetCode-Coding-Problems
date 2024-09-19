bea2d60d102dfac4a9bf9a145e3f892cdcbb1d73
Sharvil21 <sshastri2102@gmail.com>
2024-09-19 15:28:54 -0400

    Adding the Pandas Solution

--- Solutions/Easy/184_Department_Highest_Salary_Pandas_Solution.py
+++ Solutions/Easy/184_Department_Highest_Salary_Pandas_Solution.py
@@ -1,1 +1,6 @@
 #Pandas Solution
+import pandas as pd
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    df = employee.merge(department, left_on='departmentId',right_on='id',how='inner')
+    df['rnk'] = df.groupby('name_y')['salary'].rank(method='dense',ascending=False)
