62d78bb9399b4ccab6a6d80e8263748d0cd4888e
Sharvil21 <sshastri2102@gmail.com>
2024-09-21 15:43:30 -0400

    Added the Pandas Solution

--- Solutions/Easy/178_Rank_Scores_Pandas_Solution.py
+++ Solutions/Easy/178_Rank_Scores_Pandas_Solution.py
@@ -1,1 +1,6 @@
 #Pandas Solution
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores['rank'] = scores['score'].rank(method='dense',ascending=False)
+    return scores[['score','rank']]
