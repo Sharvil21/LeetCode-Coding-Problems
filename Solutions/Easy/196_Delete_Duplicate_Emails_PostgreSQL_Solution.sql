bd0526d92b66083447830627771f3649c5edb0a0
Sharvil21 <sshastri2102@gmail.com>
2024-09-22 15:57:23 -0400

    Added another PostgreSQL Solution

--- Solutions/Easy/196_Delete_Duplicate_Emails_PostgreSQL_Solution.sql
+++ Solutions/Easy/196_Delete_Duplicate_Emails_PostgreSQL_Solution.sql
@@ -10,3 +10,12 @@ DELETE FROM PERSON
 WHERE id IN (SELECT * FROM cte)
 
 --2nd PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+min(id) AS id, email
+FROM Person
+GROUP BY email)
+
+DELETE
+FROM Person
+WHERE id NOT IN (SELECT id FROM cte)
