caace5bb141f898adb06371b0b9962c5dfbf22cf
Sharvil21 <sshastri2102@gmail.com>
2024-09-23 16:59:03 -0400

    Added the Pandas Solution

--- Solutions/Easy/196_Delete_Duplicate_Emails_Pandas_Solution.py
+++ Solutions/Easy/196_Delete_Duplicate_Emails_Pandas_Solution.py
@@ -4,3 +4,5 @@ import pandas as pd
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values(by='id',inplace=True)
     person.drop_duplicates('email',inplace=True)
+
+#Here, we sort the values first in place, to check for test cases that have disordered ids, then drop the duplicates using the .drop_duplicates() method in-place
