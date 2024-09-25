4687dbd1c093cc9680463ba672b23dab30173834
Sharvil21 <sshastri2102@gmail.com>
2024-09-25 17:56:25 -0400

    Added the MySQL Solution

--- Solutions/Easy/1907_Count_Salary_Categories_MySQL_Solution.sql
+++ Solutions/Easy/1907_Count_Salary_Categories_MySQL_Solution.sql
@@ -6,3 +6,7 @@ UNION ALL
 SELECT
 'Average Salary' AS category, SUM(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END) AS accounts_count
 FROM Accounts
+UNION ALL
+SELECT
+'High Salary' AS category, SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
+FROM Accounts
