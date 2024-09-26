4c0ec5db3e5f8887a4ffa0e1f3bb800fa046b933
Sharvil21 <sshastri2102@gmail.com>
2024-09-26 18:00:27 -0400

    Adding the second PostgreSQL Solution

--- Solutions/Easy/1907_Count_Salary_Categories_PostgreSQL_Solution.sql
+++ Solutions/Easy/1907_Count_Salary_Categories_PostgreSQL_Solution.sql
@@ -13,3 +13,12 @@ SELECT
 FROM Accounts
 
 --2nd PostgreSQL Solution
+WITH cte1 AS (SELECT account_id,
+CASE WHEN income < 20000 THEN 'Low Salary'
+WHEN income >= 20000 AND income <= 50000 THEN 'Average Salary'
+WHEN income > 50000 THEN 'High Salary' END AS category
+FROM Accounts
+UNION SELECT NULL, 'Low Salary'
+UNION SELECT NULL, 'Average Salary'
+UNION SELECT NULL, 'High Salary'
+)
