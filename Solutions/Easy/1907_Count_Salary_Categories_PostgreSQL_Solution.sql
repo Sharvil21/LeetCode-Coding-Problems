4929e8d1967e999f68052204bfd743fcfa31626d
Sharvil21 <sshastri2102@gmail.com>
2024-09-26 17:58:20 -0400

    Adding the PostgreSQL Solution

--- Solutions/Easy/1907_Count_Salary_Categories_PostgreSQL_Solution.sql
+++ Solutions/Easy/1907_Count_Salary_Categories_PostgreSQL_Solution.sql
@@ -1,1 +1,5 @@
---PostgreSQL Solutio
+--PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+SELECT
+'Low Salary' AS category, SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS accounts_count
+FROM Accounts
