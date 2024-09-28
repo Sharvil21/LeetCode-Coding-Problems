56bee7ecfb613c4f2acda153876cd907aae9dac5
Sharvil21 <sshastri2102@gmail.com>
2024-09-28 18:39:45 -0400

    Added the Pandas Solution

--- Solutions/Easy/1741_Find_Total_Time_Spent_by_Each_Employee_Pandas_Solution.py
+++ Solutions/Easy/1741_Find_Total_Time_Spent_by_Each_Employee_Pandas_Solution.py
@@ -3,3 +3,4 @@ import pandas as pd
 
 def total_time(employees: pd.DataFrame) -> pd.DataFrame:
     employees['total_time'] = employees['out_time'] - employees['in_time']
+    return employees.groupby(['emp_id','event_day'])['total_time'].sum().reset_index()[['event_day','emp_id','total_time']].rename(columns = {'event_day':'day'})
