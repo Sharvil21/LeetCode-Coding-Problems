4c28f788b38591478818413ac2f711f0910aa3a4
Sharvil21 <sshastri2102@gmail.com>
2024-09-30 18:02:36 -0400

    Adding the second Pandas Solution

--- Solutions/Easy/2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher_Pandas_Solution.py
+++ Solutions/Easy/2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher_Pandas_Solution.py
@@ -5,3 +5,4 @@ def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
     return teacher.groupby('teacher_id')['subject_id'].nunique().to_frame('cnt').reset_index()
 
 #Second Pandas Solution (Using .agg() method)
+import pandas as pd
