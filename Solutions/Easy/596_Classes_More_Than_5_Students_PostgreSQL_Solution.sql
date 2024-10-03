1494956c91928886b290dd5ae49002593e13e5ea
Sharvil21 <sshastri2102@gmail.com>
2024-10-02 20:57:18 -0400

    Adding the PostgreSQL Solution

--- Solutions/Easy/596_Classes_More_Than_5_Students_PostgreSQL_Solution.sql
+++ Solutions/Easy/596_Classes_More_Than_5_Students_PostgreSQL_Solution.sql
@@ -1,1 +1,8 @@
 --PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+class, COUNT(student) AS cnt
+FROM Courses
+GROUP BY 1)
+SELECT class FROM cte WHERE cnt >=5
+
