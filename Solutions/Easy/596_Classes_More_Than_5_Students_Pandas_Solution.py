5eaf1ff512ca7011e31f7c7d56b4a49e8206042f
Sharvil21 <sshastri2102@gmail.com>
2024-10-02 20:53:34 -0400

    Added the correct Pandas Solution

--- Solutions/Easy/596_Classes_More_Than_5_Students_Pandas_Solution.py
+++ Solutions/Easy/596_Classes_More_Than_5_Students_Pandas_Solution.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     df = courses.groupby('class')['student'].count().to_frame('Total_Students').reset_index()
-    return df.query("Total_Students > 5")[['class']]
+    return df.query("Total_Students >= 5")[['class']]
 
 #Another Pandas Solution
 import pandas as pd
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby('class')['student'].count().to_frame('Total Students').reset_index()
+    return df.loc[df['Total Students']>= 5,['class']]
