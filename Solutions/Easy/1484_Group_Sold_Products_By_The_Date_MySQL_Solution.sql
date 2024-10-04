98ec61c005e6d9bb107b0030f886b62fe004f08d
Sharvil21 <sshastri2102@gmail.com>
2024-10-03 21:07:55 -0400

    Added the MySQL Solution

--- Solutions/Easy/1484_Group_Sold_Products_By_The_Date_MySQL_Solution.sql
+++ Solutions/Easy/1484_Group_Sold_Products_By_The_Date_MySQL_Solution.sql
@@ -1,3 +1,9 @@
 --MySQL Solution (Have to use the GROUP_CONCAT() Function to get the desired 3rd column)
+SELECT
+sell_date,
+COUNT(DISTINCT product) AS num_sold,
+GROUP_CONCAT(DISTINCT product ORDER BY product ASC) AS products
+FROM Activities
+GROUP BY 1;
 
 
