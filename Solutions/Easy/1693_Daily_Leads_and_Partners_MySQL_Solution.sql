948688fa6fb8fefd059d0fd2f5e38e71cb12888b
Sharvil21 <sshastri2102@gmail.com>
2024-10-05 21:23:09 -0400

    Added the MySQL Solution

--- Solutions/Easy/1693_Daily_Leads_and_Partners_MySQL_Solution.sql
+++ Solutions/Easy/1693_Daily_Leads_and_Partners_MySQL_Solution.sql
@@ -1,7 +1,12 @@
 --MySQL Solution
 SELECT
 date_id, make_name,
-COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS partner_id
+COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS Unique_partners
 FROM DailySales
 GROUP BY 1,2;
+
+
+
+
+
 
