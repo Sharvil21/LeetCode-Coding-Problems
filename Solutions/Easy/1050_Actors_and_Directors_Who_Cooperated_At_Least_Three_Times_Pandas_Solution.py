672fe0db8655c45e5276bd9d5759853f100c42ef
Sharvil21 <sshastri2102@gmail.com>
2024-10-06 01:56:11 -0400

    Adding another Pandas Solution

--- Solutions/Easy/1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times_Pandas_Solution.py
+++ Solutions/Easy/1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times_Pandas_Solution.py
@@ -4,3 +4,5 @@ import pandas as pd
 def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
     df = actor_director.groupby(['actor_id','director_id'])['timestamp'].count().to_frame('total_collabs').reset_index()
     return df.loc[df['total_collabs']>2,['actor_id','director_id']]
+
+#
