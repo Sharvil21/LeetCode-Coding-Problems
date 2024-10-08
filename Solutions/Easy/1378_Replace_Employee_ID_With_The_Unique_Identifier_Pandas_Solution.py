060951b16337a9e210ee1f3d92189e9f2f63448f
Sharvil21 <sshastri2102@gmail.com>
2024-10-08 13:24:17 -0400

    Added the Pandas Solution

--- Solutions/Easy/1378_Replace_Employee_ID_With_The_Unique_Identifier_Pandas_Solution.py
+++ Solutions/Easy/1378_Replace_Employee_ID_With_The_Unique_Identifier_Pandas_Solution.py
@@ -1,4 +1,7 @@
 #Pandas Solution
 import pandas as pd
 def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    df = employees.merge(employee_uni,on='id',how='left')
+    return df.replace(float('nan'),None)[['name','unique_id']]
+
 
