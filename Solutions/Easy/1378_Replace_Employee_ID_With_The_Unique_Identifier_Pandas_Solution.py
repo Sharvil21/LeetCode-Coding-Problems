58635b46e352c96880975b9d3016cf23d2fc352c
Sharvil21 <sshastri2102@gmail.com>
2024-10-08 13:25:41 -0400

    Adding another Pandas Solution

--- Solutions/Easy/1378_Replace_Employee_ID_With_The_Unique_Identifier_Pandas_Solution.py
+++ Solutions/Easy/1378_Replace_Employee_ID_With_The_Unique_Identifier_Pandas_Solution.py
@@ -4,4 +4,4 @@ def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> 
     df = employees.merge(employee_uni,on='id',how='left')
     return df.replace(float('nan'),None)[['name','unique_id']]
 
-#
+#2nd Pandas solution (Without using .replace() method. NaN is considered as Null in the test cases)
