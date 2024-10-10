1c58a2bb496d76e9bef64bd35ba038afb99f3c3d
Sharvil21 <sshastri2102@gmail.com>
2024-10-10 15:27:12 -0400

    Added another PostgreSQL solution

--- Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_PostgreSQL_Solution.sql
+++ Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_PostgreSQL_Solution.sql
@@ -7,3 +7,20 @@ WHERE
 id IN (SELECT managerId FROM Employee GROUP BY 1 HAVING COUNT(*) > 4 )
 
 -- 2nd PostgreSQL solution
+-- Write your PostgreSQL query statement below
+
+
+WITH ManagerReports AS
+
+(
+    SELECT managerId , COUNT(managerId) AS managerReports
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+)
+
+SELECT e.name
+FROM Employee e 
+INNER JOIN ManagerReports mr
+ON e.id = mr.managerId
+WHERE mr.managerReports >= 5; 
