1d9ec479afa72c028f941bf3f91536175e4a42b3
Sharvil21 <sshastri2102@gmail.com>
2024-10-10 15:29:44 -0400

    Added the MySQL Solution

--- Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_MySQL_Solution.sql
+++ Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_MySQL_Solution.sql
@@ -1,1 +1,10 @@
 --MySQL Solution
+WITH cte1 AS (SELECT
+a.id, a.name, a.managerid, b.name AS manager_name
+FROM Employee a
+JOIN Employee b ON a.managerId = b.id),
+cte2 AS (SELECT managerid, manager_name, COUNT(managerid) AS cnt FROM cte1
+GROUP BY 1,2)
+
+SELECT manager_name AS "name" FROM cte2
+WHERE cnt > 4
