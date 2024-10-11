b49339d9a1aa3ce6a7378b94f334710363a486c3
Sharvil21 <sshastri2102@gmail.com>
2024-10-11 15:48:33 -0400

    Adding the Pandas Solution

--- Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_Pandas_Solution.py
+++ Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_Pandas_Solution.py
@@ -1,4 +1,4 @@
 #Pandas Solution
 import pandas as pd
 def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
-
+	df = employee.groupby('managerId')['id'].count().to_frame('total_reports').reset_index()
