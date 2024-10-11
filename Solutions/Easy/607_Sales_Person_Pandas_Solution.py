fd620a2509fff2452979b89fb1161bcb6c690549
Sharvil21 <sshastri2102@gmail.com>
2024-10-11 16:20:21 -0400

    Added the Pandas Solution

--- Solutions/Easy/607_Sales_Person_Pandas_Solution.py
+++ Solutions/Easy/607_Sales_Person_Pandas_Solution.py
@@ -2,4 +2,8 @@
 import pandas as pd
 
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    merged_df1 = sales_person.merge(orders,on='sales_id', how = 'left')
+    final_df = merged_df1.merge(company,on='com_id',how='left')
+    names_to_be_excluded = final_df.loc[final_df['name_y']=='RED','name_x']
+    return sales_person.query("name not in @names_to_be_excluded")['name'].to_frame('name')
 
