1e811e7ebda586f5d199e0307750123598ea2d4c
Sharvil21 <sshastri2102@gmail.com>
2024-10-12 16:03:21 -0400

    Added the correct solution for this problem

--- Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_Pandas_Solution.py
+++ Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_Pandas_Solution.py
@@ -1,2 +1,7 @@
 #Pandas Solution
 import pandas as pd
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.groupby('managerId')['id'].count().to_frame('total_reports').reset_index()
+    managers = df.loc[df['total_reports']>=5,'managerId']
+    return employee.query("id in @managers")['name'].to_frame('name')
