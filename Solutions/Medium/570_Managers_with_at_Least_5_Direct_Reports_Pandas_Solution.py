31a036bce792c6ae88146a25e1bfb51ef0af9a70
Sharvil21 <sshastri2102@gmail.com>
2024-10-12 16:02:10 -0400

    Added the Pandas Solution

--- Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_Pandas_Solution.py
+++ Solutions/Medium/570_Managers_with_at_Least_5_Direct_Reports_Pandas_Solution.py
@@ -1,6 +1,2 @@
 #Pandas Solution
 import pandas as pd
-def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
-	df = employee.groupby('managerId')['id'].count().to_frame('total_reports').reset_index()
-	managers = df['managerId']
-	return employee.query("id in @managers")[['name']]
