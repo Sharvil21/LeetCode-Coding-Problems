1631c7077ea5f3d33347f3e936f24e4725233aa8
Sharvil21 <sshastri2102@gmail.com>
2024-10-14 16:34:05 -0400

    Added the MySQL Solution

--- Solutions/Easy/607_Sales_Person_MySQL_Solution.py
+++ Solutions/Easy/607_Sales_Person_MySQL_Solution.py
@@ -1,1 +1,12 @@
 --MySQL Solution
+WITH cte AS (SELECT
+SalesPerson.name
+FROM SalesPerson
+LEFT JOIN Orders USING(sales_id)
+LEFT JOIN Company USING(com_id)
+WHERE Company.name = "RED")
+
+SELECT
+name
+FROM SalesPerson
+WHERE name NOT IN (SELECT * FROM CTE)
