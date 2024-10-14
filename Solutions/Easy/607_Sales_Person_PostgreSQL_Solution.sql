938fd67d571d141e8fc199b3d434a137eedb1f82
Sharvil21 <sshastri2102@gmail.com>
2024-10-14 16:35:07 -0400

    Added the PostgreSQL Solution

--- Solutions/Easy/607_Sales_Person_PostgreSQL_Solution.sql
+++ Solutions/Easy/607_Sales_Person_PostgreSQL_Solution.sql
@@ -1,2 +1,13 @@
 --PostgreSQL Solution
+WITH cte AS (SELECT
+SalesPerson.name
+FROM SalesPerson
+LEFT JOIN Orders USING(sales_id)
+LEFT JOIN Company USING(com_id)
+WHERE Company.name = "RED")
+
+SELECT
+name
+FROM SalesPerson
+WHERE name NOT IN (SELECT * FROM CTE)
 
