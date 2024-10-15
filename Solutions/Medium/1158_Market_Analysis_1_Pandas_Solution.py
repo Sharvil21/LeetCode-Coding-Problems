624de93a71efba01a4a947d50b94d11cc38c4082
Sharvil21 <sshastri2102@gmail.com>
2024-10-15 16:26:38 -0400

    Adding another Pandas Solution

--- Solutions/Medium/1158_Market_Analysis_1_Pandas_Solution.py
+++ Solutions/Medium/1158_Market_Analysis_1_Pandas_Solution.py
@@ -6,4 +6,4 @@ def market_analysis(users: pd.DataFrame, orders: pd.DataFrame, items: pd.DataFra
     df1['order_in_2019'] = df1['order_date'].dt.year == 2019
     return df1.groupby(['user_id','join_date']).agg(orders_in_2019=('order_in_2019','sum')).reset_index().rename(columns={'user_id':'buyer_id'})
 
-#2nd Pandas SOlution
+#2nd Pandas Solution
