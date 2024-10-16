b811ba53883dd4abc6ac9d7aaef98428d4f6ee43
Sharvil21 <sshastri2102@gmail.com>
2024-10-16 16:42:25 -0400

    Adding the PostgreSQL Solution

--- Solutions/Medium/3475_DNA_Pattern_Recognition_PostgreSQL_Solution.sql
+++ Solutions/Medium/3475_DNA_Pattern_Recognition_PostgreSQL_Solution.sql
@@ -1,3 +1,12 @@
 --PostgreSQL Solution
 -- Write your PostgreSQL query statement below
-
+-- Write your PostgreSQL query statement below
+SELECT
+sample_id, dna_sequence, species,
+SUM(CASE WHEN dna_sequence ~* '^ATG' THEN 1 ELSE 0 END) AS has_start,
+SUM(CASE WHEN dna_sequence ~* '(TAA|TAG|TGA)$' THEN 1 ELSE 0 END) AS has_stop,
+SUM(CASE WHEN dna_sequence ~* 'ATAT' THEN 1 ELSE 0 END) AS has_atat,
+SUM(CASE WHEN dna_sequence ~* '(G{3}|G{4})' THEN 1 ELSE 0 END) AS has_ggg
+FROM Samples
+GROUP BY 1,2,3
+ORDER BY 1
