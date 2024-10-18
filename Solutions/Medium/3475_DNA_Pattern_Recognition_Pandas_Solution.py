2e6588630a14f736744e182187f3cc8874cec8c2
Sharvil21 <sshastri2102@gmail.com>
2024-10-18 16:58:02 -0400

    Added the Pandas Solution

--- Solutions/Medium/3475_DNA_Pattern_Recognition_Pandas_Solution.py
+++ Solutions/Medium/3475_DNA_Pattern_Recognition_Pandas_Solution.py
@@ -1,2 +1,10 @@
 #Pandas Solution
 #Write the Pandas Query Below
+import pandas as pd
+
+def analyze_dna_patterns(samples: pd.DataFrame) -> pd.DataFrame:
+    samples['has_start'] = samples['dna_sequence'].str.contains("^ATG").astype(int)
+    samples['has_stop'] = samples['dna_sequence'].str.contains("(TAA|TAG|TGA)$").astype(int)
+    samples['has_atat'] = samples['dna_sequence'].str.contains("ATAT").astype(int)
+    samples['has_ggg'] = samples['dna_sequence'].str.contains("(G{4}|G{3})").astype(int)
+    return samples
