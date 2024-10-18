4a561899d6318ade5c6817f0009acfbbb69dacc6
Sharvil21 <sshastri2102@gmail.com>
2024-10-18 17:10:22 -0400

    Added another Pandas Solution

--- Solutions/Medium/3475_DNA_Pattern_Recognition_Pandas_Solution.py
+++ Solutions/Medium/3475_DNA_Pattern_Recognition_Pandas_Solution.py
@@ -10,3 +10,12 @@ def analyze_dna_patterns(samples: pd.DataFrame) -> pd.DataFrame:
     return samples
 
 #Pandas Solution
+SELECT sample_id,
+       dna_sequence,
+       species,
+       dna_sequence ~ '^ATG'           AS has_start,
+       dna_sequence ~ 'TAA$|TAG$|TGA$' AS has_stop,
+       dna_sequence ~ '.*ATAT.*'       AS has_atat,
+       dna_sequence ~ '.*GGG.*'        AS has_ggg
+  FROM samples
+ ORDER BY sample_id;
