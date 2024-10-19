8a048d321cfb9384fd1ab9a56ef1f69bf1960924
Sharvil21 <sshastri2102@gmail.com>
2024-10-19 15:10:56 -0400

    Adding another Pandas Solution

--- Solutions/Easy/1084_Sales_Analysis_3_Pandas_Solution.py
+++ Solutions/Easy/1084_Sales_Analysis_3_Pandas_Solution.py
@@ -5,3 +5,5 @@ def sales_analysis(product: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
     df1 = product.merge(sales,on='product_id',how='inner')
     unrequired = df1.query("sale_date < '2019-01-01' or sale_date > '2019-03-31'")['product_id']
     return df1.query("product_id not in @unrequired")[['product_id','product_name']].drop_duplicates()
+
+#
