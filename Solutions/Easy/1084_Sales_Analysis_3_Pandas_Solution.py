ea69fa57af902394fde94d1dfb578d6b465469f3
Sharvil21 <sshastri2102@gmail.com>
2024-10-19 15:11:35 -0400

    Added another Pandas Solution

--- Solutions/Easy/1084_Sales_Analysis_3_Pandas_Solution.py
+++ Solutions/Easy/1084_Sales_Analysis_3_Pandas_Solution.py
@@ -7,3 +7,12 @@ def sales_analysis(product: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
     return df1.query("product_id not in @unrequired")[['product_id','product_name']].drop_duplicates()
 
 #2nd Pandas Solution
+import pandas as pd
+
+def sales_analysis(product: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
+
+    sales = sales.groupby('product_id')['sale_date'].agg(['min', 'max']).reset_index()
+    #min column: Contains the earliest sale date for each product.
+    #max column: Contains the latest sale date for each product.
+    return pd.merge(product, sales).query('min >= "2019-01-01" and max <= "2019-03-31"')[['product_id', 'product_name']]
+
