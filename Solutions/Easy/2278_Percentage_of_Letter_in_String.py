93de56e090dda1f66344bf91b897785e82c32bcf
Sharvil21 <sshastri2102@gmail.com>
2024-10-21 18:11:21 -0400

    Added the Python Solution

--- Solutions/Easy/2278_Percentage_of_Letter_in_String.py
+++ Solutions/Easy/2278_Percentage_of_Letter_in_String.py
@@ -1,1 +1,9 @@
 #Python Solution using floor division
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        count = 0
+        for i in s:
+            if i == letter:
+                count += 1
+
+        return 100*count//len(s)
