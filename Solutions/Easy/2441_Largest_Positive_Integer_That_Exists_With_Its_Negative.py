33919128ea588a090cc186c54561d383889a0ec2
Sharvil21 <sshastri2102@gmail.com>
2024-10-21 14:14:28 -0400

    Added the Brute force Python Solution

--- Solutions/Easy/2441_Largest_Positive_Integer_That_Exists_With_Its_Negative.py
+++ Solutions/Easy/2441_Largest_Positive_Integer_That_Exists_With_Its_Negative.py
@@ -1,1 +1,8 @@
 #Brute Force Solution
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        largest = -1
+        for i in sorted(nums):
+            if -i in nums:
+                largest = i
+        return largest
