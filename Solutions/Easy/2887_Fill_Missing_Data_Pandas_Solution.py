742161c63f4570429148b9784d7eee44c0c65803
Sharvil21 <sshastri2102@gmail.com>
2024-10-23 11:41:39 -0400

    Added another Pandas Solution

--- Solutions/Easy/2887_Fill_Missing_Data_Pandas_Solution.py
+++ Solutions/Easy/2887_Fill_Missing_Data_Pandas_Solution.py
@@ -7,5 +7,9 @@ def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
 
 
 #Another Pandas Solution using .fillna() method
+import pandas as pd
 
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products['quantity'] = products['quantity'].fillna(0)
+    return products
 
