7adb40309308f2d1d8eb505c6772eb0ddda8da05
Sharvil21 <sshastri2102@gmail.com>
2024-10-24 12:25:47 -0400

    Added the Python Solution

--- Solutions/Easy/2085_Count_Common_Words_With_One_Occurrence_Python_Solution.py
+++ Solutions/Easy/2085_Count_Common_Words_With_One_Occurrence_Python_Solution.py
@@ -1,2 +1,15 @@
 #Python Solution using Counter
+from collections import Counter
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        counter1 = Counter(words1)
+        counter2 = Counter(words2)
+        cnt = 0
+        for i in counter1:
+            if i in counter2:
+                if counter1[i] == 1 and counter2[i] == 1:
+                    cnt += 1
+
+        return cnt
+
 
