428925b618ea1b3e99db80b7655d7aafe46f45a5
Sharvil21 <sshastri2102@gmail.com>
2024-10-25 12:57:11 -0400

    Added the MySQL Solution

--- Solutions/Easy/182_Duplicate_Emails_MySQL_Solution.sql
+++ Solutions/Easy/182_Duplicate_Emails_MySQL_Solution.sql
@@ -1,2 +1,10 @@
 --MySQL Solution
 --Write your MySQL Query Statement Below
+WITH cte AS (SELECT
+email, COUNT(email) AS total_emails
+FROM Person
+GROUP BY 1
+HAVING COUNT(email) > 1)
+
+SELECT email AS Email
+FROM cte
