93b4b615b5e8734a84e96fac0605457c926becb0
Sharvil21 <sshastri2102@gmail.com>
2024-10-26 13:02:42 -0400

    Added the first PostgreSQL Solution

--- Solutions/Easy/182_Duplicate_Emails_PostgreSQL_Solution.sql
+++ Solutions/Easy/182_Duplicate_Emails_PostgreSQL_Solution.sql
@@ -1,1 +1,10 @@
 --PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+email, COUNT(email) AS total_emails
+FROM Person
+GROUP BY 1
+HAVING COUNT(email) > 1)
+
+SELECT email AS Email
+FROM cte
