1d6cac6b131958b59ed6f3ec082e6191db101cb9
Sharvil21 <sshastri2102@gmail.com>
2024-10-28 13:52:17 -0400

    Modified the Pandas Solution

--- Solutions/Easy/182_Duplicate_Emails_Pandas_Solution.py
+++ Solutions/Easy/182_Duplicate_Emails_Pandas_Solution.py
@@ -1,7 +1,9 @@
---Pandas Solution using .groupby() method
+#Pandas Solution using .groupby() method
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
     df2 = person.groupby(by='email')['email'].count().to_frame('cnt')
     df3 = df2.loc[df2['cnt']>1].reset_index().rename(columns={'email':'Email'})
     return df3[['Email']]
+
+
