af3e4e952dd3173acce61f7c1224acd06dd9603b
Sharvil21 <sshastri2102@gmail.com>
2024-10-30 13:10:51 -0400

    Added the Pandas Solution

--- Solutions/Easy/181_Employees_Earning_More_Than_Their_Managers_Pandas_Solution.py
+++ Solutions/Easy/181_Employees_Earning_More_Than_Their_Managers_Pandas_Solution.py
@@ -1,1 +1,6 @@
 #Pandas Solution using .merge() method (Basically self join)
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.merge(employee,left_on='managerId',right_on='id',how='inner')
+    return df.loc[df['salary_x']>df['salary_y'],'name_x'].to_frame('Employee')
