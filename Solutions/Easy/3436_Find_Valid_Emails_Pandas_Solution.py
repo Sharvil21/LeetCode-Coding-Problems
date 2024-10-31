ea51e4dcb745d295a979ad4281e795c4434fdb71
Sharvil21 <sshastri2102@gmail.com>
2024-10-31 16:14:50 -0400

    Addedd the Pandas Solution

--- Solutions/Easy/3436_Find_Valid_Emails_Pandas_Solution.py
+++ Solutions/Easy/3436_Find_Valid_Emails_Pandas_Solution.py
@@ -1,2 +1,6 @@
 #Pandas Solution
 import pandas as pd
+import pandas as pd
+
+def find_valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    return users.loc[users['email'].str.contains('^[A-Za-z0-9_]*@[A-Za-z]*.com')]
