5b574154f90ceb780c81bff9635745b69fe72ce4
Sharvil21 <sshastri2102@gmail.com>
2024-11-01 16:27:07 -0400

    Added another PostgreSQL Solution

--- Solutions/Easy/3436_Find_Valid_Emails_PostgreSQL_Solution.py
+++ Solutions/Easy/3436_Find_Valid_Emails_PostgreSQL_Solution.py
@@ -7,3 +7,9 @@ WHERE
 email ~ '^[A-Za-z0-9_]*@[A-Za-z]*.com$'
 
 --Another PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+SELECT
+*
+FROM Users
+WHERE
+email ~ '^[\w]*@[A-Za-z]*.com$'
