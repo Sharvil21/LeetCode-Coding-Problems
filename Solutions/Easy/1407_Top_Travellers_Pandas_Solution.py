0db6182300c71bc088a532840cbc3b291052b90a
Sharvil21 <sshastri2102@gmail.com>
2024-11-03 11:54:16 -0500

    Added the Pandas Solution

--- Solutions/Easy/1407_Top_Travellers_Pandas_Solution.py
+++ Solutions/Easy/1407_Top_Travellers_Pandas_Solution.py
@@ -1,1 +1,7 @@
 #Pandas Solution using .merge(). Basically do a LEFT JOIN, then group by user_id and name, then take the sum. Rename the columns. Then sort by the 2 values
+import pandas as pd
+
+def top_travellers(users: pd.DataFrame, rides: pd.DataFrame) -> pd.DataFrame:
+    df = users.merge(rides,left_on='id',right_on='user_id',how='left').fillna(0)
+    df2 = df.groupby(['user_id','name'])['distance'].sum().reset_index()
+    return df2[['name','distance']].rename(columns={'distance':'travelled_distance'}).sort_values(['travelled_distance','name'],ascending=[False,True])
