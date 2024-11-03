15ea332f483dd2e04ac9f0f324ae3a9228810a06
Sharvil21 <sshastri2102@gmail.com>
2024-11-03 11:53:53 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1407_Top_Travellers_Pandas_Solution.py
+++ Solutions/Easy/1407_Top_Travellers_Pandas_Solution.py
@@ -1,1 +1,1 @@
-#Pandas Solution using .merge().
+#Pandas Solution using .merge(). Basically do a LEFT JOIN, then group by user_id and name, then take the sum. Rename the columns. Then sort by the 2 values
