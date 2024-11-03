973c42fd198002794c9d78dd9294120c623eba03
Sharvil21 <sshastri2102@gmail.com>
2024-11-03 11:58:43 -0500

    Adding another Pandas Solution

--- Solutions/Easy/1407_Top_Travellers_Pandas_Solution.py
+++ Solutions/Easy/1407_Top_Travellers_Pandas_Solution.py
@@ -5,3 +5,5 @@ def top_travellers(users: pd.DataFrame, rides: pd.DataFrame) -> pd.DataFrame:
     df = users.merge(rides,left_on='id',right_on='user_id',how='left').fillna(0)
     df2 = df.groupby(['user_id','name'])['distance'].sum().reset_index()
     return df2[['name','distance']].rename(columns={'distance':'travelled_distance'}).sort_values(['travelled_distance','name'],ascending=[False,True])
+
+#
