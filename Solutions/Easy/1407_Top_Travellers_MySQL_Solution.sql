9695eb5d840094d4d2d6daeaae238f8606a424b5
Sharvil21 <sshastri2102@gmail.com>
2024-11-04 12:06:10 -0500

    Added the MySQL Solution

--- Solutions/Easy/1407_Top_Travellers_MySQL_Solution.sql
+++ Solutions/Easy/1407_Top_Travellers_MySQL_Solution.sql
@@ -1,1 +1,12 @@
 --MySQL Solution using left join and CASE WHEN
+WITH cte AS (SELECT
+u.id AS id, u.name AS name, SUM(CASE WHEN r.distance IS NOT NULL THEN r.distance ELSE 0 END) AS travelled_distance
+FROM
+Users u LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY 1,2
+ORDER BY 2 DESC, 1 ASC)
+SELECT
+name, travelled_distance
+FROM cte
+ORDER BY travelled_distance DESC, name ASC
