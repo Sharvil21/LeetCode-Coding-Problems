de06c592c44157b8be0df522841e63115ef50c24
Sharvil21 <sshastri2102@gmail.com>
2024-11-04 12:07:02 -0500

    Added another MySQL Solution

--- Solutions/Easy/1407_Top_Travellers_MySQL_Solution.sql
+++ Solutions/Easy/1407_Top_Travellers_MySQL_Solution.sql
@@ -12,3 +12,14 @@ FROM cte
 ORDER BY travelled_distance DESC, name ASC
 
 --Another MYSQL Solution
+# Write your MySQL query statement below
+with v as (
+    select user_id, sum(distance) as distance
+    from Rides
+    group by user_id
+)
+
+select name, ifnull(distance, 0) as travelled_distance
+from Users u
+left join v on u.id = v.user_id
+order by travelled_distance desc, name
