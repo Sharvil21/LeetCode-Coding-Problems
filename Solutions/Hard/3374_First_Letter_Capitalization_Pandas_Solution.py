82eee3de57ebbe88e6b34c978871542eb97fee2d
Sharvil21 <sshastri2102@gmail.com>
2024-11-06 15:15:19 -0500

    Adding another Pandas Solution

--- Solutions/Hard/3374_First_Letter_Capitalization_Pandas_Solution.py
+++ Solutions/Hard/3374_First_Letter_Capitalization_Pandas_Solution.py
@@ -5,4 +5,4 @@ def capitalize_content(user_content: pd.DataFrame) -> pd.DataFrame:
     user_content['converted_text'] = user_content['content_text'].str.title()
     return user_content.rename(columns={'content_text':'original_text'})
 
-#Another
+#Another Pandas Solution
