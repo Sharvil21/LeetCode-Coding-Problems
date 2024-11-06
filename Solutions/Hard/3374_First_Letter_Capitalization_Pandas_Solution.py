9f47d36d18d239ed2142bc0f72bedf50011bbdaf
Sharvil21 <sshastri2102@gmail.com>
2024-11-06 15:14:29 -0500

    Added the Pandas Solution

--- Solutions/Hard/3374_First_Letter_Capitalization_Pandas_Solution.py
+++ Solutions/Hard/3374_First_Letter_Capitalization_Pandas_Solution.py
@@ -1,1 +1,6 @@
 #Pandas Solution using .title() method directly to create a new column
+import pandas as pd
+
+def capitalize_content(user_content: pd.DataFrame) -> pd.DataFrame:
+    user_content['converted_text'] = user_content['content_text'].str.title()
+    return user_content.rename(columns={'content_text':'original_text'})
