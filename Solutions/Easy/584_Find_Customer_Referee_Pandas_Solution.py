24604e93eaf85a79d2ccbf2cbf066d4d3c731b35
Sharvil21 <sshastri2102@gmail.com>
2024-11-08 16:44:42 -0500

    Added the Pandas Solution

--- Solutions/Easy/584_Find_Customer_Referee_Pandas_Solution.py
+++ Solutions/Easy/584_Find_Customer_Referee_Pandas_Solution.py
@@ -1,1 +1,5 @@
 #Pandas Solution
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    return customer.query("referee_id != 2 or referee_id.isna()")['name'].to_frame()
