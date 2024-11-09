fa5f2e8234458d8f4b3dba45cb6f8500f512fcb9
Sharvil21 <sshastri2102@gmail.com>
2024-11-09 16:53:27 -0500

    Adding another Pandas Solution

--- Solutions/Easy/584_Find_Customer_Referee_Pandas_Solution.py
+++ Solutions/Easy/584_Find_Customer_Referee_Pandas_Solution.py
@@ -10,4 +10,4 @@ import pandas as pd
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
     return customer.loc[(customer['referee_id']!= 2) | (customer['referee_id'].isna()),'name'].to_frame()
 
-#Another
+#Another Pandas Solution
