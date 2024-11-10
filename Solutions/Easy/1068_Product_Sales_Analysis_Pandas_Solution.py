a41f2e7cf4a7e0ae6deca17e9def5772e42dfbec
Sharvil21 <sshastri2102@gmail.com>
2024-11-10 16:59:11 -0500

    Adding another Pandas Solution

--- Solutions/Easy/1068_Product_Sales_Analysis_Pandas_Solution.py
+++ Solutions/Easy/1068_Product_Sales_Analysis_Pandas_Solution.py
@@ -6,12 +6,4 @@ def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
     return df[['product_name','year','price']]
 
 #Another Pandas Solution - 1 liner, beats 99%
-import pandas as pd
 
-def sales_analysis(sales: pd.DataFrame, 
-                   product: pd.DataFrame) -> pd.DataFrame:
-
-    return pd.merge(sales, product).iloc[:,[5,2,4]]
-
-
-#
