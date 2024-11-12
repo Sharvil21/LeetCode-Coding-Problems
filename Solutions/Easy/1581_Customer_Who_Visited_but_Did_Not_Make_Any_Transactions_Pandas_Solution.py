59b42c9f1f6a8a1a8a90a38f6f7ff19d58bad5f0
Sharvil21 <sshastri2102@gmail.com>
2024-11-12 17:15:44 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
+++ Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
@@ -1,2 +1,2 @@
 #Pandas Solution
-#Join the two tables using .merge() method first. Then use .groupby() method to group by customer_id, then use .agg() method for convenience. 
+#Join the two tables using .merge() method first. Then use .groupby() method to group by customer_id, then use .agg() method for convenience. Take the 'count'. rename the columns and return the output.
