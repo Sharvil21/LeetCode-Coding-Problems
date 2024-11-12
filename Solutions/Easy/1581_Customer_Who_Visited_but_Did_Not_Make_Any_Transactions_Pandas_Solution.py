ca332f692ce6e5ba5c49dac2921c30e669fa5d68
Sharvil21 <sshastri2102@gmail.com>
2024-11-12 17:15:54 -0500

    Added the Pandas Solution

--- Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
+++ Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
@@ -1,2 +1,8 @@
 #Pandas Solution
 #Join the two tables using .merge() method first. Then use .groupby() method to group by customer_id, then use .agg() method for convenience. Take the 'count'. rename the columns and return the output.
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    df = visits.merge(transactions,on='visit_id',how='left')
+    op = df.query("transaction_id.isna()").groupby('customer_id').agg(count_no_trans=('customer_id','count'))
+    return op.reset_index().sort_values(by='count_no_trans',ascending=False)
