e112d3368cb57b6ab80c83157ae90ea1d20b20f8
Sharvil21 <sshastri2102@gmail.com>
2024-11-12 17:20:39 -0500

    Adding another Pandas Solution

--- Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
+++ Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
@@ -6,3 +6,5 @@ def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataF
     df = visits.merge(transactions,on='visit_id',how='left')
     op = df.query("transaction_id.isna()").groupby('customer_id').agg(count_no_trans=('customer_id','count'))
     return op.reset_index().sort_values(by='count_no_trans',ascending=False)
+
+#
