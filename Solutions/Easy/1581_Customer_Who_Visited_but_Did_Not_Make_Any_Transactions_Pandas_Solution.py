badda69110ca4676061aba5a41a8d355661a4855
Sharvil21 <sshastri2102@gmail.com>
2024-11-13 17:21:06 -0500

    Added another Pandas Solution

--- Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
+++ Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
@@ -8,3 +8,12 @@ def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataF
     return op.reset_index().sort_values(by='count_no_trans',ascending=False)
 
 #Another Pandas Solution
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    
+    df = visits.merge(transactions, how='left')
+
+    df = df[df['transaction_id'].isna()].groupby(['customer_id'], as_index=False).agg(count_no_trans=('visit_id', 'nunique'))
+
+    return df
