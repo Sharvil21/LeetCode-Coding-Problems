45b235ec77d92fe88652ee46f97fad420e775fdd
Sharvil21 <sshastri2102@gmail.com>
2024-11-13 17:29:43 -0500

    Adding 3rd another Pandas Solution

--- Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
+++ Solutions/Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions_Pandas_Solution.py
@@ -17,3 +17,5 @@ def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataF
     df = df[df['transaction_id'].isna()].groupby(['customer_id'], as_index=False).agg(count_no_trans=('visit_id', 'nunique'))
 
     return df
+
+#
