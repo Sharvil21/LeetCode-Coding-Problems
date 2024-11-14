36f422f0afa8b541596c1b405255caaa49d4d0af
Sharvil21 <sshastri2102@gmail.com>
2024-11-14 18:00:47 -0500

    Added the Pandas Solution

--- Solutions/Easy/197_Rising_Temperature_Pandas_Solution.py
+++ Solutions/Easy/197_Rising_Temperature_Pandas_Solution.py
@@ -1,1 +1,7 @@
 #Pandas Solution
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather.sort_values(by='recordDate',inplace=True)
+    weather['previous_temperature'] = weather['temperature'].shift(1)
+    return weather.query("temperature > previous_temperature").reset_index()[['id']]
