7673f5dc0d592a1dd8b39cbab05197d31a4bcf88
Sharvil21 <sshastri2102@gmail.com>
2024-11-15 16:41:05 -0500

    Adding the Pandas Solution

--- Solutions/Easy/577_Employee_Bonus_Pandas_Solution.py
+++ Solutions/Easy/577_Employee_Bonus_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution: 
 #Step 1: Merge the tables using a left Join
 #Step 2: Filter out the row where bonus is <1000 Or where the bonus column is null
+#For null values, we have to use .isna() method, and not '== None' as this won't work
