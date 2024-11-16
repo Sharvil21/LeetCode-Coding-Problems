037a5ea160e7a2b97318e5e4286ee5deec885d97
Sharvil21 <sshastri2102@gmail.com>
2024-11-16 16:44:59 -0500

    Adding another Pandas Solution

--- Solutions/Easy/577_Employee_Bonus_Pandas_Solution.py
+++ Solutions/Easy/577_Employee_Bonus_Pandas_Solution.py
@@ -10,4 +10,4 @@ def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
      df = employee.merge(bonus,on='empId', how = 'left')
      return df.loc[(df['bonus'] < 1000) | (df['bonus'].isna()),['name','bonus']]
 
-#
+#Another Pandas Solution
