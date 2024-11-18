99e6a462b18b8f6bc5f784b1a0f8852d280cd10d
Sharvil21 <sshastri2102@gmail.com>
2024-11-17 21:59:14 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1934_Confirmation_Rate_Pandas_Solution.py
+++ Solutions/Medium/1934_Confirmation_Rate_Pandas_Solution.py
@@ -2,3 +2,4 @@
 #Step 1: Merge signups and confirmations on user_id using a left join
 #Step 2 : use .fillna(0) method to fill NaN values
 #Step 3: Filter only the confirmed messages then group by user_id and then count the occurrences
+#Step 4: Group by user_id and count all messages (df_total).
