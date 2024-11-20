79014b9ed85ecb7142729f53685d8c2a9f6060ea
Sharvil21 <sshastri2102@gmail.com>
2024-11-20 13:33:27 -0500

    Added the Pandas Solution

--- Solutions/Easy/1075_Project_Employees_1_Pandas_Solution.py
+++ Solutions/Easy/1075_Project_Employees_1_Pandas_Solution.py
@@ -1,2 +1,6 @@
 #Pandas Solution
+import pandas as pd
 
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    df = project.merge(employee,on='employee_id',how='inner')
+    return df.groupby('project_id')['experience_years'].mean().round(2).reset_index().rename(columns={'experience_years':'average_years'})
