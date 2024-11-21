f5a51dd47ae16d94192920a12ec858dec97a3e0f
Sharvil21 <sshastri2102@gmail.com>
2024-11-21 13:38:41 -0500

    Added another Pandas Solution

--- Solutions/Easy/1075_Project_Employees_1_Pandas_Solution.py
+++ Solutions/Easy/1075_Project_Employees_1_Pandas_Solution.py
@@ -10,5 +10,14 @@ def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.Dat
     return df.groupby('project_id')['experience_years'].mean().round(2).reset_index().rename(columns={'experience_years':'average_years'})
 
 #Another Pandas Solution - clean code, uses .rename() method
+import pandas as pd
 
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+  # merge DataFrames 'project' and 'employee' 
+  df = pd.merge(project, employee, how='left', on='employee_id')
+
+  # group by 'project_id' and find the mean of the 'experience_years'
+  result = df.groupby('project_id')['experience_years'].mean().round(2).rename('average_years').reset_index()
+
+  return result
 
