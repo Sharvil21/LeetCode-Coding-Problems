b9be19307f14fef6ee27f984073afc27582ca2c5
Sharvil21 <sshastri2102@gmail.com>
2024-11-22 14:23:51 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1633_Percentage_of_Users_Attended_a_Contest_Pandas_Solution.py
+++ Solutions/Easy/1633_Percentage_of_Users_Attended_a_Contest_Pandas_Solution.py
@@ -1,3 +1,4 @@
 # Pandas Solution
 #First merge the two dataframes
 #Then get the total no. of participants independently in a different variable
+#Group the merged DF by contest id and count the total users for each contest
