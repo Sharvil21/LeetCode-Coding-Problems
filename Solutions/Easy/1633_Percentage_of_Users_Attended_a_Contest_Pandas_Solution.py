381d6846e105d052639acb2147a322f35a93faa6
Sharvil21 <sshastri2102@gmail.com>
2024-11-22 14:59:02 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1633_Percentage_of_Users_Attended_a_Contest_Pandas_Solution.py
+++ Solutions/Easy/1633_Percentage_of_Users_Attended_a_Contest_Pandas_Solution.py
@@ -2,4 +2,5 @@
 #First merge the two dataframes
 #Then get the total no. of participants independently in a different variable
 #Group the merged DF by contest id and count the total users for each contest
-#
+#Create a new column called 'percentage' to get the percentage. The values will be 100* total_registered participants divided by the total participants
+
