8f7bc297278115b66ec96d030a2083f033b374b7
Sharvil21 <sshastri2102@gmail.com>
2024-11-23 13:27:07 -0500

    Added the MySQL Solution

--- Solutions/Easy/1211_Queries_Quality_and_Percentage_MySQL_Solution.sql
+++ Solutions/Easy/1211_Queries_Quality_and_Percentage_MySQL_Solution.sql
@@ -1,1 +1,9 @@
 --MySQL Solution
+SELECT
+query_name,
+ROUND(AVG(rating/position),2) AS quality,
+ROUND(AVG(CASE WHEN rating<3 THEN 100 ELSE 0 END),2) AS poor_query_percentage
+FROM Queries
+GROUP BY 1;
+
+
