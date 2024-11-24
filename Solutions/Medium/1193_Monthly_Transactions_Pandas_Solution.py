4f79154f23dbc62084497db8fdc4dd7a71e9e9c4
Sharvil21 <sshastri2102@gmail.com>
2024-11-24 13:53:47 -0500

    Added the Pandas Solution

--- Solutions/Medium/1193_Monthly_Transactions_Pandas_Solution.py
+++ Solutions/Medium/1193_Monthly_Transactions_Pandas_Solution.py
@@ -1,2 +1,11 @@
 #Pandas Solution
 import pandas as pd
+import pandas as pd
+
+def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
+    transactions['month'] = transactions['trans_date'].dt.year.astype(str) + '-' + transactions['trans_date'].dt.month.astype(str).str.zfill(2)
+    
+    transactions['approved_total_count'] = transactions['state'].apply(lambda x:1 if x == 'approved' else 0)
+    transactions['approved_total_amount'] = transactions.apply(lambda x:x['amount'] if x['state'] == 'approved' else 0,axis = 1)
+    return transactions.groupby(['month','country'],dropna=False).agg(trans_count=('id','count'),approved_count=('approved_total_count','sum'),trans_total_amount=('amount','sum'),approved_total_amount=('approved_total_amount','sum')).reset_index()
+
