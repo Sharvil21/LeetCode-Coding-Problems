04809604a0ecf42bf6ca3a2ffc7a6d6baefe2f12
Sharvil21 <sshastri2102@gmail.com>
2024-11-25 14:04:20 -0500

    Added another Pandas Solution

--- Solutions/Medium/1193_Monthly_Transactions_Pandas_Solution.py
+++ Solutions/Medium/1193_Monthly_Transactions_Pandas_Solution.py
@@ -10,3 +10,12 @@ def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
 
 #Another Pandas Solution
 #This time, using .dt.strftime("%Y-%m") to get the month column 
+import pandas as pd
+
+def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
+    transactions['month'] = transactions['trans_date'].dt.strftime("%Y-%m")
+    transactions['approved_total_count'] = transactions['state'].apply(lambda x:1 if x == 'approved' else 0)
+    transactions['approved_total_amount'] = transactions.apply(lambda x:x['amount'] if x['state'] == 'approved' else 0,axis = 1)
+    return transactions.groupby(['month','country'],dropna=False).agg(trans_count=('id','count'),approved_count=('approved_total_count','sum'),trans_total_amount=('amount','sum'),approved_total_amount=('approved_total_amount','sum')).reset_index()
+
+
