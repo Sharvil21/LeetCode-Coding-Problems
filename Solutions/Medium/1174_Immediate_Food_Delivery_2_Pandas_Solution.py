1ac3d4da0533d741ce03bcb0cd8c3af3dfe07360
Sharvil21 <sshastri2102@gmail.com>
2024-11-26 14:39:50 -0500

    Added the Pandas Solution

--- Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
+++ Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
@@ -1,2 +1,2 @@
 #Pandas Solution
-#First, get the min order date. In Pandas, .transform('min') will have to be used
+#First, get the min order date. In Pandas, .transform('min') will have to be used so that we get first_order_date in all rows of the dataframe
