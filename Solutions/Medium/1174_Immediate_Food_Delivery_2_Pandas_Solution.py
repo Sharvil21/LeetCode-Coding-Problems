834015ca1dfaaa7173a1cec02cc1cf665cca633b
Sharvil21 <sshastri2102@gmail.com>
2024-11-26 14:40:23 -0500

    Added the Pandas Solution

--- Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
+++ Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #First, get the min order date. In Pandas, .transform('min') will have to be used so that we get first_order_date in all rows of the dataframe
+#Then create another columns called condition_satisfied
