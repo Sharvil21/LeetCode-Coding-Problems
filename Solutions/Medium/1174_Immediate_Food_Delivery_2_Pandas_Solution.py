290942c1bdddf8b512b9d8c7a64a202c8d4aefd6
Sharvil21 <sshastri2102@gmail.com>
2024-11-27 14:42:49 -0500

    Added the Pandas Solution

--- Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
+++ Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
@@ -4,3 +4,11 @@
 #NOw all we have to do is get the sum of the condition_satisfied column and the number of unique customers using .nunique() method
 #Lastly, we have to create a dataframe so as to return the output
 #Since we're passing only scalar value in the dataframe we have to specify the index
+
+import pandas as pd
+
+def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    delivery['first_order_date'] = delivery.groupby('customer_id')['order_date'].transform('min')
+    delivery['condition_satisfied'] = delivery.apply(lambda x:1 if x['customer_pref_delivery_date'] == x['first_order_date'] else 0, axis = 1)
+    value = (delivery['condition_satisfied'].sum())/(delivery['customer_id'].nunique())
+    return pd.DataFrame({'immediate_percentage':100*value},index=[0]).round(2)
