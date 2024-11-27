0bd18de79f5335e4672060070399ecff218260eb
Sharvil21 <sshastri2102@gmail.com>
2024-11-27 14:42:40 -0500

    Added the Pandas Solution

--- Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
+++ Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
@@ -3,3 +3,4 @@
 #Then create another columns called condition_satisfied. Assign them values of 1 or 0 based on whether first_order_date is the same as the customer_pref_delivery_date
 #NOw all we have to do is get the sum of the condition_satisfied column and the number of unique customers using .nunique() method
 #Lastly, we have to create a dataframe so as to return the output
+#Since we're passing only scalar value in the dataframe we have to specify the index
