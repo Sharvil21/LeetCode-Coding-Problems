523855033512ef0b9be5581e3df9c8ce380193d6
Sharvil21 <sshastri2102@gmail.com>
2024-11-27 14:40:56 -0500

    Added the Pandas Solution

--- Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
+++ Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
@@ -1,3 +1,3 @@
 #Pandas Solution
 #First, get the min order date. In Pandas, .transform('min') will have to be used so that we get first_order_date in all rows of the dataframe
-#Then create another columns called condition_satisfied
+#Then create another columns called condition_satisfied. Assign them values of 1 or 0 based on whether first_order_date is the same as the customer_pref_delivery_date
