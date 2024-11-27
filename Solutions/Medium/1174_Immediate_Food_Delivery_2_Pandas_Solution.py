7b560073e10e33e723d94ec283a56c390260d717
Sharvil21 <sshastri2102@gmail.com>
2024-11-27 14:41:31 -0500

    Added the Pandas Solution

--- Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
+++ Solutions/Medium/1174_Immediate_Food_Delivery_2_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #First, get the min order date. In Pandas, .transform('min') will have to be used so that we get first_order_date in all rows of the dataframe
 #Then create another columns called condition_satisfied. Assign them values of 1 or 0 based on whether first_order_date is the same as the customer_pref_delivery_date
+#NOw all we have to do is get the sum of the condition_satisfied column and the number of unique customers using .nunique() method
