4ffb08c9742d035645c9c3e7ac59da990c7663ae
Sharvil21 <sshastri2102@gmail.com>
2024-11-28 16:16:08 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1141_User_Activity_for_the_Past_30_Days_1_Pandas_Solution.py
+++ Solutions/Easy/1141_User_Activity_for_the_Past_30_Days_1_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #Step 1: Get only the rows which staisfy the condition of falling within 30 days of 2019-07-27
+#For this, use the pd.to_datetime() function or pd.Timedelta(days=30) methods
