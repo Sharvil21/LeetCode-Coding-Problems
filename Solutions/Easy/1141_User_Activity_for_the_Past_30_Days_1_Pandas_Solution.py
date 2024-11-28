8aed5d5640a1a8c4b11f62db9424ece090001886
Sharvil21 <sshastri2102@gmail.com>
2024-11-28 16:16:58 -0500

    Added the Pandas Solution

--- Solutions/Easy/1141_User_Activity_for_the_Past_30_Days_1_Pandas_Solution.py
+++ Solutions/Easy/1141_User_Activity_for_the_Past_30_Days_1_Pandas_Solution.py
@@ -2,3 +2,8 @@
 #Step 1: Get only the rows which staisfy the condition of falling within 30 days of 2019-07-27
 #For this, use the pd.to_datetime() function or pd.Timedelta(days=30) methods
 #Then group by the activity_date, then get the no. of unique users for each day using .nunique() method
+import pandas as pd
+
+def user_activity(activity: pd.DataFrame) -> pd.DataFrame:
+    df = activity[(activity['activity_date'] <= pd.to_datetime('2019-07-27')) & (activity['activity_date'] > (pd.to_datetime('2019-07-27') - pd.Timedelta(days=30)))]
+    return df.groupby('activity_date')['user_id'].nunique().reset_index().rename(columns={'activity_date':'day','user_id':'active_users'})
