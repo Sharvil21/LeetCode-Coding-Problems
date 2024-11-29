5a3944eb118b404e88873f0d77aaeb20c82f45f7
Sharvil21 <sshastri2102@gmail.com>
2024-11-29 16:38:32 -0500

    Created the .py file for Pandas Solution

--- Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
+++ Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
@@ -3,3 +3,4 @@
 #Sort the values just in case
 #Then create a column called rnk. The logic here is to rank the rows partitioned by each product_id for every year. Dense rank will be used, and the first year be will ranked 1.
 #Then filter out those rows only where the rnk value is 1
+#Finally return the desired columns, don't forget to rename them
