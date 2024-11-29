e88711e037915a45086c882151911b0c9b7de9ec
Sharvil21 <sshastri2102@gmail.com>
2024-11-29 16:38:54 -0500

    Added the Pandas Solution

--- Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
+++ Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
@@ -4,3 +4,11 @@
 #Then create a column called rnk. The logic here is to rank the rows partitioned by each product_id for every year. Dense rank will be used, and the first year be will ranked 1.
 #Then filter out those rows only where the rnk value is 1
 #Finally return the desired columns, don't forget to rename them
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    df = sales.merge(product,on='product_id',how='inner')
+    df.sort_values(by=['product_id','year'],inplace=True)
+    df['rnk'] = df.groupby('product_id')['year'].rank(method='dense',ascending=True)
+    return df.loc[df['rnk']==1,['product_id','year','quantity','price']].rename(columns={'year':'first_year'})
+
