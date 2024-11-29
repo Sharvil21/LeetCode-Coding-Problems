fd10249193622403a9147f51cb701853f02ac8b4
Sharvil21 <sshastri2102@gmail.com>
2024-11-29 16:37:37 -0500

    Created the .py file for Pandas Solution

--- Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
+++ Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
@@ -1,4 +1,4 @@
 #Pandas Solution 
 #First Join the two tables usiong .merge()
 #Sort the values just in case
-#Then create a column called rnk. The logic here is to rank the rows partitioned by each product_id for every year.  
+#Then create a column called rnk. The logic here is to rank the rows partitioned by each product_id for every year. Dense rank will be used, and the first year be will ranked 1.
