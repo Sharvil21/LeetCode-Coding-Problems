50e84407c4bb52a87cfdf0979f0901e832ae4364
Sharvil21 <sshastri2102@gmail.com>
2024-11-30 16:55:41 -0500

    Added another Pandas Solution

--- Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
+++ Solutions/Medium/1070_Product_Sales_Analysis_3_Pandas_Solution.py
@@ -20,3 +20,10 @@ def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
 #Put those values in a separate column in the dataframe
 #Now filter out the rows where the year == first_year (basically the min year value). No need to drop dupicate values here
 #return the desired columns for the output
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    df = sales.merge(product,on='product_id',how='inner')
+    df['first_year'] = df.groupby('product_id')['year'].transform('min')
+    final = df[df['year']==df['first_year']]
+    return final[['product_id','first_year','quantity','price']]
