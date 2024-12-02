02c1ac57164805efccf34fc08addc2f5bea6ddac
Sharvil21 <sshastri2102@gmail.com>
2024-12-02 14:41:48 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1045_Customers_Who_Bought_All_Products_Pandas_Solution.py
+++ Solutions/Medium/1045_Customers_Who_Bought_All_Products_Pandas_Solution.py
@@ -2,3 +2,4 @@
 #Merge the two tables first using a left join using .merge() method
 #Group the df then by customer_id based on the number of unique products they bought. (Designated by the product_key column)
 #Then get the total unique products in the products table
+#Lastly, compare the two values. We need to get the customer_id where the total_products_bought by each customer id is the same as the total number of unique products in the products df
