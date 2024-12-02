1ea7922a7876098cbc3ee6777f8bfa90edf14335
Sharvil21 <sshastri2102@gmail.com>
2024-12-02 14:40:25 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1045_Customers_Who_Bought_All_Products_Pandas_Solution.py
+++ Solutions/Medium/1045_Customers_Who_Bought_All_Products_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #Merge the two tables first using a left join using .merge() method
+#Group the df then by customer_id based on the number of unique products they bought. (Designated by the product_key column)
