93dc2459043e0f214e04d8d18e311096a986cf20
Sharvil21 <sshastri2102@gmail.com>
2024-12-03 15:13:29 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1789_Primary_Department_for_Each_Employee_Pandas_Solution.py
+++ Solutions/Easy/1789_Primary_Department_for_Each_Employee_Pandas_Solution.py
@@ -1,2 +1,9 @@
 #Pandas Solution
 import pandas as pd
+import pandas as pd
+
+def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
+    
+    employee.sort_values(by=['employee_id','primary_flag'],ascending=[True,False],inplace=True)
+    employee.drop_duplicates(subset=['employee_id'],keep='first',inplace=True)
+    return employee[['employee_id','department_id']]
