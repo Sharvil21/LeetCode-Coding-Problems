84ccb5b9f8117bc1ee3f5d44d6b26cd4ec6f34ae
Sharvil21 <sshastri2102@gmail.com>
2024-12-04 15:16:11 -0500

    Adding a one liner Pandas Solution

--- Solutions/Easy/1789_Primary_Department_for_Each_Employee_Pandas_Solution.py
+++ Solutions/Easy/1789_Primary_Department_for_Each_Employee_Pandas_Solution.py
@@ -8,3 +8,5 @@ def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
     employee.sort_values(by=['employee_id','primary_flag'],ascending=[True,False],inplace=True)
     employee.drop_duplicates(subset=['employee_id'],keep='first',inplace=True)
     return employee[['employee_id','department_id']]
+
+#
