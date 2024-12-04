17917f334e65a6172660d0a130ab2da061975e17
Sharvil21 <sshastri2102@gmail.com>
2024-12-04 15:26:24 -0500

    Adding 3rd Pandas Solution

--- Solutions/Easy/1789_Primary_Department_for_Each_Employee_Pandas_Solution.py
+++ Solutions/Easy/1789_Primary_Department_for_Each_Employee_Pandas_Solution.py
@@ -16,3 +16,5 @@ import pandas as pd
 def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
     
     return employee.sort_values(by=['employee_id','primary_flag'],ascending=[True,False]).drop_duplicates(subset=['employee_id'],keep='first')[['employee_id','department_id']]
+
+#
