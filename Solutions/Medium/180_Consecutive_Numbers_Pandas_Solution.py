ae2b18cc9f4953a9b74c831586b7a9f70146d24e
Sharvil21 <sshastri2102@gmail.com>
2024-12-05 15:40:40 -0500

    Adding the Pandas Solution

--- Solutions/Medium/180_Consecutive_Numbers_Pandas_Solution.py
+++ Solutions/Medium/180_Consecutive_Numbers_Pandas_Solution.py
@@ -3,3 +3,7 @@
 #For this, we can use .shift() method
 
 import pandas as pd
+import pandas as pd
+
+def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
+    return logs.loc[(logs['num']==logs['num'].shift(-1)) & (logs['num'] == logs['num'].shift(-2)),'num'].drop_duplicates(keep='first').to_frame('ConsecutiveNums')
