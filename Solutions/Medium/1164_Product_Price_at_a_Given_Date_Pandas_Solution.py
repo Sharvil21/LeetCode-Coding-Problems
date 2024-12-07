22ecdbfc07449ad93c632aeb600c50709bd3aa55
Sharvil21 <sshastri2102@gmail.com>
2024-12-06 20:18:57 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
+++ Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
@@ -1,3 +1,3 @@
 #Pandas Solution
-#First, sort the values by product_id and change_date ascending. 
+i#First, sort the values by product_id and change_date ascending. This will ensure that all testcases will pass with ease 
 import pandas as pd
