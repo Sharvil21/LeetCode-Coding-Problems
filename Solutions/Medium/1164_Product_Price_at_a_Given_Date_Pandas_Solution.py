01b300ddace8f52268db46c6e441776f3ef8b976
Sharvil21 <sshastri2102@gmail.com>
2024-12-06 20:19:46 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
+++ Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
-i#First, sort the values by product_id and change_date ascending. This will ensure that all testcases will pass with ease 
+#First, sort the values by product_id and change_date ascending. This will ensure that all testcases will pass with ease
+#Then, get only those rows for product_id where the change date is <= 2019-08-16
 import pandas as pd
