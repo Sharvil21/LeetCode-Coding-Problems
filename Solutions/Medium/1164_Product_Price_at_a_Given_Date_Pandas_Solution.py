5c7a49b03c1d0d69a8a3c55240f5e07aebb910cd
Sharvil21 <sshastri2102@gmail.com>
2024-12-07 20:21:45 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
+++ Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
@@ -2,5 +2,5 @@
 #First, sort the values by product_id and change_date ascending. This will ensure that all testcases will pass with ease
 #Then, get only those rows for product_id where the change date is <= 2019-08-16
 #Drop the duplicates and keep only the last value.
-#Create another dataframe from the original (sorted one obviously) and drop the duplicates. This is for the
+#Create another dataframe from the original (sorted one obviously) and drop the duplicates. This is for the test cases where the product_id change_date is always graeter than 2019-08-16.
 import pandas as pd
