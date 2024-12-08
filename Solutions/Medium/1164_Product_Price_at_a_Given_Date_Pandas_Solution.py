16333b242ed0d0ca7fcea20aaf59655dc70853e4
Sharvil21 <sshastri2102@gmail.com>
2024-12-07 20:23:02 -0500

    Adding the Pandas Solution

--- Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
+++ Solutions/Medium/1164_Product_Price_at_a_Given_Date_Pandas_Solution.py
@@ -4,4 +4,5 @@
 #Drop the duplicates and keep only the last value.
 #Create another dataframe from the original (sorted one obviously) and drop the duplicates. This is for the test cases where the product_id change_date is always graeter than 2019-08-16.
 #Do a Left JOIN with the first data frame and the second one on product_id
+#The changed_price will be NaN for the dates which don't exist before the required date
 import pandas as pd
