a07937958a177af8df549effc13bb3537392ca11
Sharvil21 <sshastri2102@gmail.com>
2024-12-08 14:52:13 -0500

    Adding the MySQL Solution

--- Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_MySQL_Solution.sql
+++ Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_MySQL_Solution.sql
@@ -1,3 +1,4 @@
 --MySQL Solution
 --First, use SUM() OVER() window function to get the cumulative sum for each turn
 --Put that in a cte
+--Then select only that person name when ordering the cumulative weight in descending order
