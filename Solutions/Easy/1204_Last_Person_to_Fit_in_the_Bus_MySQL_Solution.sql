9fecc6248ab3cacedbe0c433491de0e54dc03f20
Sharvil21 <sshastri2102@gmail.com>
2024-12-08 14:53:05 -0500

    Added the MySQL Solution

--- Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_MySQL_Solution.sql
+++ Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_MySQL_Solution.sql
@@ -3,3 +3,16 @@
 --Put that in a cte
 --Then select only that person name when ordering the cumulative weight in descending order & the cumulative weight is <=1000
 --Lastly, only return 1 row, which will be done by LIMIT 1
+
+# Write your MySQL query statement below
+WITH cte AS (SELECT
+*,
+SUM(weight) OVER(ORDER BY turn ASC) AS weight_till_this_turn
+FROM Queue)
+
+SELECT
+person_name
+FROM cte
+WHERE weight_till_this_turn <= 1000
+ORDER BY weight_till_this_turn DESC
+LIMIT 1
