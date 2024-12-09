18b8b136dd1e31c4c7592be645cd23f379608437
Sharvil21 <sshastri2102@gmail.com>
2024-12-09 15:08:48 -0500

    Added the Pandas Solution

--- Solutions/Easy/1978_Employees_Whose_Manager_Left_the_Company_Pandas_Solution.py
+++ Solutions/Easy/1978_Employees_Whose_Manager_Left_the_Company_Pandas_Solution.py
@@ -3,3 +3,7 @@
 #We can use .loc[] and .isin() method to make it work
 #So basically, we filter out saying give us the rows where manager_id is not in employee_id column and also the manager_id is not null and also where the salary is <30000
 #Lastly, sort the values by employee_id
+import pandas as pd
+
+def find_employees(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees.loc[(~employees['manager_id'].isin(employees['employee_id'])) & (~employees['manager_id'].isna()) & (employees['salary'] <30000),'employee_id'].to_frame('employee_id').sort_values('employee_id')
