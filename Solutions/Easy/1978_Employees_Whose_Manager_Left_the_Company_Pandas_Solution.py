d15e8516137c317f237e53a034a501306500254b
Sharvil21 <sshastri2102@gmail.com>
2024-12-09 15:08:33 -0500

    Adding the Pandas Solution

--- Solutions/Easy/1978_Employees_Whose_Manager_Left_the_Company_Pandas_Solution.py
+++ Solutions/Easy/1978_Employees_Whose_Manager_Left_the_Company_Pandas_Solution.py
@@ -2,3 +2,4 @@
 #Logic is that we need to find the manager_id which does not exist in the employee_id column
 #We can use .loc[] and .isin() method to make it work
 #So basically, we filter out saying give us the rows where manager_id is not in employee_id column and also the manager_id is not null and also where the salary is <30000
+#Lastly, sort the values by employee_id
