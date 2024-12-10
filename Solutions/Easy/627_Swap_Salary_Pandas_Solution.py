f2d85d5ece3d2fbb431ac4e365ca0824b8ae088a
Sharvil21 <sshastri2102@gmail.com>
2024-12-10 17:18:19 -0500

    Added the Pandas Solution

--- Solutions/Easy/627_Swap_Salary_Pandas_Solution.py
+++ Solutions/Easy/627_Swap_Salary_Pandas_Solution.py
@@ -1,2 +1,7 @@
 #Pandas Solution
 #First solution: Using .replace mnethod
+import pandas as pd
+
+def swap_salary(salary: pd.DataFrame) -> pd.DataFrame:
+    salary['sex'].replace({'f':'m','m':'f'},inplace=True)
+    return salary
