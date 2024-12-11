6ec0369d822f822754bcde693a1c3b601fe524f7
Sharvil21 <sshastri2102@gmail.com>
2024-12-11 17:24:28 -0500

    Adding another Pandas Solution

--- Solutions/Easy/627_Swap_Salary_Pandas_Solution.py
+++ Solutions/Easy/627_Swap_Salary_Pandas_Solution.py
@@ -14,4 +14,9 @@ def swap_salary(salary: pd.DataFrame) -> pd.DataFrame:
     return salary.replace({'f':'m','m':'f'})
 
 #Using if else condition
+import pandas as pd
 
+def swap_salary(salary: pd.DataFrame) -> pd.DataFrame:
+    salary['sex'] = ['m' if x=='f' else 'f' for x in salary.sex]
+    return salary
+    
