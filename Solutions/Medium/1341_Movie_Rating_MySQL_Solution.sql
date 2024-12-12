069f1f49b43e8bbb882563600953fa4d828e9cd9
Sharvil21 <sshastri2102@gmail.com>
2024-12-12 16:13:01 -0500

    Added the MySQL Solution

--- Solutions/Medium/1341_Movie_Rating_MySQL_Solution.sql
+++ Solutions/Medium/1341_Movie_Rating_MySQL_Solution.sql
@@ -5,3 +5,25 @@
 -- Use the AVG() function to get the ratings. Don't forget to use WHERE clause to filter only those rows where the created_at is in FEB 2020. Sort the values.
 --Also, for both CTEs make sure to add LIMIT 1 as it causes problems when used in a UNION
 -- Then, first write a select query to get the name from the first CTE then UNION ALL then another select query to get the movie name from second CTE
+WITH cte1 AS (SELECT
+user_id, name, COUNT(movie_id) AS total_reviews
+FROM 
+Users JOIN MovieRating USING(user_id)
+GROUP BY 1,2
+ORDER BY COUNT(movie_id) DESC, name ASC LIMIT 1),
+cte2 AS (
+    SELECT title, movie_id, AVG(rating) AS average_rating
+    FROM Movies JOIN MovieRating USING(movie_id)
+    WHERE EXTRACT(YEAR FROM created_at) = 2020 AND EXTRACT(MONTH FROM created_at) = 2
+    GROUP BY 1,2
+    ORDER BY 3 DESC, 1 ASC
+    LIMIT 1
+)
+
+SELECT
+name AS results
+FROM cte1
+UNION ALL
+SELECT
+title
+FROM cte2
