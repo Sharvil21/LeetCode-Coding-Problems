38b26e580af4de3304360e43ed3ca76dd7984e72
Sharvil21 <sshastri2102@gmail.com>
2024-12-12 16:11:29 -0500

    Adding the MySQL Solution

--- Solutions/Medium/1341_Movie_Rating_MySQL_Solution.sql
+++ Solutions/Medium/1341_Movie_Rating_MySQL_Solution.sql
@@ -2,3 +2,4 @@
 --First, write a SELECT query to get the name of the review that has the highest reviews
 --This can be done by using COUNT() function along with group by. Order by this value DESCENDING and the name ASCENDING. Put this query in a CTE
 --Then, in another CTE, get the Average ratings for all movies by JOINing the movies table with MovieRating Table
+-- Use the AVG() function to get the ratings. Don't forget to use WHERE clause to filter only those rows where the created_at is in FEB 2020
