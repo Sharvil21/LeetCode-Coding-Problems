d8d8d2236a4bfeef905724a9e01e209334ba0017
Sharvil21 <sshastri2102@gmail.com>
2024-12-13 16:56:07 -0500

    Created the .py file for Pandas Solution

--- Solutions/Medium/1321_Restaurant_Growth_Pandas_Solution.py
+++ Solutions/Medium/1321_Restaurant_Growth_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #First, create another dataframe which contains the values of total amount spent each day by customers
+#Then, we have to create a column that gives the 7 day cumulative sum. We cannot use .cumsum() for this
