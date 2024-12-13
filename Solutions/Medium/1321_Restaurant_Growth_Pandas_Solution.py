30217e6ab5536dc519afa7631fdf8f4c5b6b65ab
Sharvil21 <sshastri2102@gmail.com>
2024-12-13 16:57:31 -0500

    Created the .py file for Pandas Solution

--- Solutions/Medium/1321_Restaurant_Growth_Pandas_Solution.py
+++ Solutions/Medium/1321_Restaurant_Growth_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #First, create another dataframe which contains the values of total amount spent each day by customers
 #Then, we have to create a column that gives the 7 day cumulative sum. We cannot use .cumsum() for this. WE have to use the .rolling() method. IT will have 2 arguments: windows = 7, and min_periods = 7. So that it takes the sum of the last 6 and the present value. And only those values will be considered if it has 7 rows before it
+#Same for the average amount, which will be the moving average. We will specify the same arguments.
