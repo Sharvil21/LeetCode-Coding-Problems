0dacd9518f230768bf30ef5773eb37a22c880369
Sharvil21 <sshastri2102@gmail.com>
2024-12-14 17:32:29 -0500

    Created the .py file for Pandas Solution

--- Solutions/Medium/602_Friend_Requests_2_Who_Has_the_Most_Friends_Pandas_Solution.py
+++ Solutions/Medium/602_Friend_Requests_2_Who_Has_the_Most_Friends_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #Simple approach is to do a 'union' between the requester_id and accepter_id column
+#If both values are in a single column, then we can just group by and get the count
