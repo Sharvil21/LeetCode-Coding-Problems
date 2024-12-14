9ec9d20ccbb45f633fabd7c6634a8e0383a720c1
Sharvil21 <sshastri2102@gmail.com>
2024-12-14 17:32:44 -0500

    Created the .py file for Pandas Solution

--- Solutions/Medium/602_Friend_Requests_2_Who_Has_the_Most_Friends_Pandas_Solution.py
+++ Solutions/Medium/602_Friend_Requests_2_Who_Has_the_Most_Friends_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #Simple approach is to do a 'union' between the requester_id and accepter_id column
 #If both values are in a single column, then we can just group by and get the count
+#This will directly tell use how many friends each user_id has
