cd22eb35469d3a734c24e8775c84f2c518bbe091
Sharvil21 <sshastri2102@gmail.com>
2024-12-24 11:13:03 -0500

    Adding the Pandas Solution

--- Solutions/Hard/185_Department_Top_Three_Salaries_Pandas_Solution.py
+++ Solutions/Hard/185_Department_Top_Three_Salaries_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #Create a new column called rank. Use the groupby() and rank methods to rank the employee and department based on their salary
 #Then filter out those rows where the rank value is < 4
+#Then select only those rows required in the output
