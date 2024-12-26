607e571a92dbea3ded714ae0ca3a4a7c302452ae
Sharvil21 <sshastri2102@gmail.com>
2024-12-26 11:13:41 -0500

    Added the Pandas Solution

--- Solutions/Hard/185_Department_Top_Three_Salaries_Pandas_Solution.py
+++ Solutions/Hard/185_Department_Top_Three_Salaries_Pandas_Solution.py
@@ -3,3 +3,9 @@
 #Then filter out those rows where the rank value is < 4
 #Then select only those rows required in the output
 #rename the columns at last and return the output
+import pandas as pd
+
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    df = employee.merge(department,left_on='departmentId',right_on='id',how='inner')
+    df['rank'] = df.groupby(['departmentId','name_y'])['salary'].rank(method='dense',ascending=False)
+    return df.loc[df['rank']<4,['name_y','name_x','salary']].rename(columns={'name_x':'Employee','name_y':'Department','salary':'Salary'})
