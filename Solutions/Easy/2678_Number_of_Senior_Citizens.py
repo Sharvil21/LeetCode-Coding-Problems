d1ccaddd893f376f93869b10561679f6fd68bd3a
Sharvil21 <sshastri2102@gmail.com>
2025-01-02 13:03:32 -0500

    Added the Proper Solution

--- Solutions/Easy/2678_Number_of_Senior_Citizens.py
+++ Solutions/Easy/2678_Number_of_Senior_Citizens.py
@@ -1,11 +1,14 @@
 #Solution that beats 100%
-ages = []
-count = 0
-for i in details:
-  ages.append(i[11:13])
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        ages = []
+        count = 0
+        for i in details:
+            ages.append(i[11:13])
 
-for i in ages:
-  if i>60:
-    count+=1
+        for i in ages:
+            if int(i)> 60:
+                count+=1
 
-return count
+        return count
+
