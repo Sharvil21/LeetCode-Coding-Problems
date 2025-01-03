c8ab5393d5ffcf881adf1e1865356a9e5f310989
Sharvil21 <sshastri2102@gmail.com>
2025-01-03 14:26:19 -0500

    Added the solution

--- Solutions/Easy/2778_Sum_of_Squares_of_Special_Elements.py
+++ Solutions/Easy/2778_Sum_of_Squares_of_Special_Elements.py
@@ -1,2 +1,13 @@
 ##Solution: Beats 100%. Use .insert() method to add a 'dummy' placeholder at index 0 in the list. Then use for loop, range(1,len(nums)):
 
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.insert(0,'dummy')
+        specials = []
+        for i in range(1,len(nums)):
+            if n%i == 0:
+                specials.append(nums[i])
+
+        return sum([i**2 for i in specials])
+
