007d82716853fe3d102459052d482e3d35708f10
Sharvil21 <sshastri2102@gmail.com>
2025-01-06 16:26:17 -0500

    Added the PostgreSQL  Solution

--- Solutions/Easy/1667_Fix_Names_in_a_Table.sql
+++ Solutions/Easy/1667_Fix_Names_in_a_Table.sql
@@ -5,3 +5,8 @@ FROM Users
 ORDER BY user_id;
 
 --PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+SELECT
+user_id, UPPER(SUBSTR(name,1,1)) || LOWER(SUBSTR(name,2)) AS name
+FROM Users
+ORDER BY user_id
