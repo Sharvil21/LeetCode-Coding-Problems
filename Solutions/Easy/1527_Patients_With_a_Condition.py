927fe2cee8a1c98a7e3e277c1461b4b45d67c6e1
Sharvil21 <sshastri2102@gmail.com>
2025-01-09 00:22:58 -0500

    Added the second Pandas solution

--- Solutions/Easy/1527_Patients_With_a_Condition.py
+++ Solutions/Easy/1527_Patients_With_a_Condition.py
@@ -6,3 +6,8 @@ def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
    
 
 #Solution 2 
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    return patients.loc[patients['conditions'].str.contains("(^|[\s])DIAB1")]
+    
