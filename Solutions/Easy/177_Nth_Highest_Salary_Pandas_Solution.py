668f0d5747ee626ed83045024ff4935c9a237fad
Sharvil21 <sshastri2102@gmail.com>
2025-01-10 14:39:51 -0500

    Added the Pandas Solution

--- Solutions/Easy/177_Nth_Highest_Salary_Pandas_Solution.py
+++ Solutions/Easy/177_Nth_Highest_Salary_Pandas_Solution.py
@@ -1,1 +1,10 @@
 #Pandas Solution using .rank() Dense Rank Method
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+
+    employee['rankk'] = employee['salary'].rank(method='dense', ascending=False)
+    if employee['salary'].drop_duplicates().shape[0] < N or N <= 0:
+        return pd.DataFrame({f'getNthHighestSalary({N})':[None]})
+
+    return employee.loc[employee['rankk']==N,'salary'].drop_duplicates().to_frame(f'getNthHighestSalary({N})')
