f0e8e15c16e48d4b846aa9e5e8b4d76282f05905
Sharvil21 <sshastri2102@gmail.com>
2025-01-10 14:15:18 -0500

    Added the postgresql Solution

--- Solutions/Easy/177_Nth_Highest_Salary_PostgreSQL_Solution.sql
+++ Solutions/Easy/177_Nth_Highest_Salary_PostgreSQL_Solution.sql
@@ -1,1 +1,15 @@
---
+--PostgreSQL Solution
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    WITH cte AS (SELECT
+    *, DENSE_RANK() OVER(ORDER BY Employee.salary DESC) AS rnk
+    FROM Employee)
+    SELECT
+    DISTINCT cte.salary
+    FROM cte WHERE rnk = n
+    
+      
+  );
+END;
+$$ LANGUAGE plpgsql;
