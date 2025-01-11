be36e4d178fa112602b3a9c24ce011b37bb208e9
Sharvil21 <sshastri2102@gmail.com>
2025-01-11 14:52:34 -0500

    Added the second Pandas Solution

--- Solutions/Easy/177_Nth_Highest_Salary_Pandas_Solution.py
+++ Solutions/Easy/177_Nth_Highest_Salary_Pandas_Solution.py
@@ -9,4 +9,12 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
 
     return employee.loc[employee['rankk']==N,'salary'].drop_duplicates().to_frame(f'getNthHighestSalary({N})')
 
-#
+#2nd Pandas Solution using .query() method instead of .loc
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    if employee['salary'].drop_duplicates().nunique() < N or N <= 0:
+        return pd.DataFrame({f'getNthHighestSalary({N})':[None]})
+    
+    employee['rnk'] = employee['salary'].rank(method='dense',ascending=False)
+    return employee.query(f'rnk == {N}')['salary'].drop_duplicates().to_frame(f'getNthHighestSalary({N})')
