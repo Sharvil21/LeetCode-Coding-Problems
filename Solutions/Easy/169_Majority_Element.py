7d7a17c0722967e7eb2d615a69f5118f04f608df
Sharvil21 <sshastri2102@gmail.com>
2025-02-11 12:47:14 -0500

    Added the 1st solution in file

---
+++ Solutions/Easy/169_Majority_Element.py
@@ -0,0 +1,14 @@
+#1st solution which leads to timeout error. But it still works with the testcases ( most of em)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(len(nums)):
+            if n == 1:
+                return nums[i]
+            word_count = 1
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    word_count += 1
+                    if word_count > n/2:
+                        return nums[i]
