d26984828ef06ec53e09d7341bad3adc3502ceb7
Sharvil21 <sshastri2102@gmail.com>
2025-02-11 12:34:46 -0500

    Added the 2nd solution

--- Solutions/Easy/217_Contains_Duplicate.py
+++ Solutions/Easy/217_Contains_Duplicate.py
@@ -5,4 +5,15 @@ class Solution:
             return False
         else:
             return True
+
+#2nd Solution (This would take a long time for large arrays/lists)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        
+        return False
+            
             
