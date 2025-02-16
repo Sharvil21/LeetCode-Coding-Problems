26f03c42402ce7008a6271d8a1b21fd2324d63ae
Sharvil21 <sshastri2102@gmail.com>
2025-02-16 12:11:47 -0500

    Made some modifications

--- Solutions/Easy/414_Third_Maximum_Number.py
+++ Solutions/Easy/414_Third_Maximum_Number.py
@@ -1,3 +1,4 @@
+#Used set() function along with sorted. Make a set of the list of numbers. Then sort it. If the length is less than 3, return the last number in that array/list/set. Else return the last third number.
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         var = sorted(set(nums))
