8df6729db87bab8a1e6198f99c87291e91e21438
Sharvil21 <sshastri2102@gmail.com>
2025-02-17 12:11:03 -0500

    Added the 2nd solution

--- Solutions/1929_Concatenation_of_Array.py
+++ Solutions/1929_Concatenation_of_Array.py
@@ -6,3 +6,8 @@ class Solution:
         for i in nums:
             ans.append(i)
         return ans
+
+#Second Solution:
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
