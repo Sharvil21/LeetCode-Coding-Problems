86787452c4d3d0aae4f3b917846bb584b3cccd90
Sharvil21 <sshastri2102@gmail.com>
2025-02-18 14:35:46 -0500

    Moved this file to this folder

--- Solutions/1920_Build_Array_from_Permutation.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        ans = nums.copy()
-        for i in range(len(nums)):
-            ans[i] = nums[nums[i]]
-
-        return ans
