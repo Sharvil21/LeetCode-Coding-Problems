244ddbe4cc8b4cc26969e261ea233bfb189fd9c8
Sharvil21 <sshastri2102@gmail.com>
2025-02-23 12:21:57 -0500

    Made modifications

--- Solutions/Easy/1859_Sorting_the_Sentence.py
+++ Solutions/Easy/1859_Sorting_the_Sentence.py
@@ -1,1 +1,14 @@
 #First Solution
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        copy = s.split()[::-1]
+        copy_2 = []
+        for i in copy:
+            copy_2.append(i[::-1])
+        output = []
+
+        for j in sorted(copy_2):
+            output.append(j[1:][::-1])
+        
+        return ' '.join(output)
+        
