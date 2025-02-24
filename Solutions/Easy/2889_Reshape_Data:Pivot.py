aeec548a31f6e02038e54c7e6772ae2d575a76e5
Sharvil21 <sshastri2102@gmail.com>
2025-02-24 12:48:08 -0500

    Added the solution

--- Solutions/Easy/2889_Reshape_Data:Pivot.py
+++ Solutions/Easy/2889_Reshape_Data:Pivot.py
@@ -1,1 +1,5 @@
 #Solution using .pivot() method of Pandas
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    return weather.pivot(index='month',columns='city',values='temperature')
