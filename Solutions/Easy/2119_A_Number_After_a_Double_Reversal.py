b43d11c6b142a1ba99e2f053591b2759854b8a0a
Sharvil21 <sshastri2102@gmail.com>
2025-02-25 18:31:04 -0500

    Added the first solution and made corrections

--- Solutions/Easy/2119_A_Number_After_a_Double_Reversal.py
+++ Solutions/Easy/2119_A_Number_After_a_Double_Reversal.py
@@ -1,5 +1,5 @@
 #First solution
-iclass Solution:
+class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
         if num%10 != 0 or num == 0:
             return (str(num)[::-1])[::-1] == str(num)
