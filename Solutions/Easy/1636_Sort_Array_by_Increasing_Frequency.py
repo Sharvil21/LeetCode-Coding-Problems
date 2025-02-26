59a8f264a92dd7be7e32b3c85e03a189e1de5436
Sharvil21 <sshastri2102@gmail.com>
2025-02-26 11:50:04 -0500

    Added the first solution

--- Solutions/Easy/1636_Sort_Array_by_Increasing_Frequency.py
+++ Solutions/Easy/1636_Sort_Array_by_Increasing_Frequency.py
@@ -1,1 +1,6 @@
-#
+#Solution
+from collections import Counter
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        return sorted(nums, key = lambda x:(count[x],-x))
