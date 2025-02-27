9e09807f71952aedc35f766d0fb237939a184b00
Sharvil21 <sshastri2102@gmail.com>
2025-02-27 14:04:59 -0500

    Adding the MySQL Solution

--- Solutions/Easy/177_Nth_Highest_Salary_MySQL_Solution.sql
+++ Solutions/Easy/177_Nth_Highest_Salary_MySQL_Solution.sql
@@ -1,1 +1,11 @@
 --Soution using DENSE_RANK()
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      WITH cte AS (SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+      FROM Employee)
+      SELECT
+      salary FROM cte WHERE rnk = n
+  );
+END
