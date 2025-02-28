4f496aaaa492eab3c3d0fd0ecddda61e633e2cfa
Sharvil21 <sshastri2102@gmail.com>
2025-02-28 13:35:26 -0500

    Added the Pandas Solution

--- Solutions/Easy/176_Second_Highest_Salary.py
+++ Solutions/Easy/176_Second_Highest_Salary.py
@@ -1,1 +1,9 @@
-#
+#Pandas Solution
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    if employee['salary'].drop_duplicates().shape[0] < 2:
+        return pd.DataFrame({'SecondHighestSalary':[None]})
+    
+    employee['rnk'] = employee['salary'].rank(method='dense',ascending=False)
+    return employee.query("rnk == 2")['salary'].drop_duplicates().to_frame("SecondHighestSalary")
