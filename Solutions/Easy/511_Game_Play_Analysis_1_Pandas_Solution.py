bdc8b29aa6cca91bbe836083b208bf6da5201b26
Sharvil21 <sshastri2102@gmail.com>
2025-03-01 16:55:54 -0500

    Adding the second Pandas Solution

--- Solutions/Easy/511_Game_Play_Analysis_1_Pandas_Solution.py
+++ Solutions/Easy/511_Game_Play_Analysis_1_Pandas_Solution.py
@@ -3,3 +3,4 @@ import pandas as pd
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     return activity.groupby('player_id')['event_date'].min().to_frame('first_login').reset_index()
 
+#
