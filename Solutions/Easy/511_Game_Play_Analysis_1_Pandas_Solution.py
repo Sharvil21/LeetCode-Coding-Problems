aa1e7d4bf23811085806f318b835c1a819d7a590
Sharvil21 <sshastri2102@gmail.com>
2025-03-01 16:56:25 -0500

    Added the second Pandas Solution

--- Solutions/Easy/511_Game_Play_Analysis_1_Pandas_Solution.py
+++ Solutions/Easy/511_Game_Play_Analysis_1_Pandas_Solution.py
@@ -4,3 +4,7 @@ def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     return activity.groupby('player_id')['event_date'].min().to_frame('first_login').reset_index()
 
 #Second Pandas Solution (Using .agg() method)
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    return activity.groupby('player_id').agg(first_login=('event_date','min')).reset_index()
