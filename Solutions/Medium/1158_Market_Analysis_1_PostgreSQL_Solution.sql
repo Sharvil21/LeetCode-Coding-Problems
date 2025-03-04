e9797698a542b2b5d4bac83fd34abeeaac6a8572
Sharvil21 <sshastri2102@gmail.com>
2025-03-04 13:36:39 -0500

    Added the PostgreSQL Solution

--- Solutions/Medium/1158_Market_Analysis_1_PostgreSQL_Solution.sql
+++ Solutions/Medium/1158_Market_Analysis_1_PostgreSQL_Solution.sql
@@ -1,1 +1,16 @@
 --PostgreSQL Solution
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+*
+FROM 
+USERS u
+LEFT JOIN Orders o ON u.user_id = o.buyer_id
+)
+
+SELECT
+user_id AS buyer_id, join_date,
+SUM(CASE WHEN EXTRACT(YEAR FROM order_date) = 2019 THEN 1 ELSE 0 END) AS orders_in_2019
+FROM cte
+GROUP BY 1,2
+ORDER By 1
+
