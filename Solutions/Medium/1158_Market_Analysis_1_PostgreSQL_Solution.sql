a10c7a4607699791e9435755a2ded2bf882b61f6
Sharvil21 <sshastri2102@gmail.com>
2025-03-04 13:59:17 -0500

    Added another PostgreSQL Solution

--- Solutions/Medium/1158_Market_Analysis_1_PostgreSQL_Solution.sql
+++ Solutions/Medium/1158_Market_Analysis_1_PostgreSQL_Solution.sql
@@ -16,3 +16,8 @@ ORDER By 1
 
 --Another PostgreSQL Solution
 
+-- Write your PostgreSQL query statement below
+SELECT user_id as buyer_id, MAX(join_date) as join_date,
+COUNT(CASE WHEN o.order_date BETWEEN '2019-01-01' AND '2019-12-31' THEN 1 ELSE NULL END) AS orders_in_2019 
+FROM Users u LEFT JOIN Orders o ON u.user_id = o.buyer_id
+GROUP BY user_id
