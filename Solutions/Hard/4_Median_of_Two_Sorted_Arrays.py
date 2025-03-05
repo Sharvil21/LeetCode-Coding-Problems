2feb419e2492b3ecb516ea4d5800fbc9de906a4d
Sharvil21 <sshastri2102@gmail.com>
2025-03-05 12:12:58 -0500

    Added the second Python Solution

--- Solutions/Hard/4_Median_of_Two_Sorted_Arrays.py
+++ Solutions/Hard/4_Median_of_Two_Sorted_Arrays.py
@@ -10,3 +10,7 @@ class Solution:
         return median_value
 
 #Second Python Solution
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        return statistics.median(nums1+nums2)
+
