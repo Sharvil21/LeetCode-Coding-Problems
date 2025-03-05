2868b7731d56995fb2a2931ee1bdec4013db3c2b
Sharvil21 <sshastri2102@gmail.com>
2025-03-05 12:11:53 -0500

    Added the First Python Solution

--- Solutions/Hard/4_Median_of_Two_Sorted_Arrays.py
+++ Solutions/Hard/4_Median_of_Two_Sorted_Arrays.py
@@ -1,1 +1,11 @@
 #Usual Python Solution
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        total_size = len(nums1) + len(nums2)
+        final_array = sorted(nums1 + nums2)
+        if total_size % 2 != 0:  # Odd case
+            median_value = final_array[total_size // 2]
+        else:  # Even case
+            median_value = (final_array[total_size // 2 - 1] + final_array[total_size // 2]) / 2
+        return median_value
+
