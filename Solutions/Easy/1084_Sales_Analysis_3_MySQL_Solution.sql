a346068cb91841bd5a99bd00f7521e24cbeda74b
Sharvil21 <sshastri2102@gmail.com>
2025-03-06 13:46:50 -0500

    Added the MySQL Solution

--- Solutions/Easy/1084_Sales_Analysis_3_MySQL_Solution.sql
+++ Solutions/Easy/1084_Sales_Analysis_3_MySQL_Solution.sql
@@ -1,2 +1,13 @@
 --MySQL Solution
 #Write your MySQL query Statement below
+WITH cte AS (SELECT
+product_id,
+product_name
+FROM Product JOIN Sales USING(product_id)
+WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31')
+
+SELECT 
+DISTINCT product_id, product_name
+FROM Product
+JOIN Sales USING(product_id)
+WHERE product_id NOT IN (SELECT product_id FROM cte)
