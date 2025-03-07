16ccecbb7621574707c0d082760bccaebb382d68
Sharvil21 <sshastri2102@gmail.com>
2025-03-07 12:32:06 -0500

    Adding the solution

--- Solutions/Easy/884_Uncommon_Words_from_Two_Sentences.py
+++ Solutions/Easy/884_Uncommon_Words_from_Two_Sentences.py
@@ -1,1 +1,1 @@
-#First Solution: Create an empty dictionary, split the strings and get the count of each word in the dictionary. Then append only those words that have a
+#First Solution: Create an empty dictionary, split the strings and get the count of each word in the dictionary. Then append only those words that have a value of 1 in an empty list
