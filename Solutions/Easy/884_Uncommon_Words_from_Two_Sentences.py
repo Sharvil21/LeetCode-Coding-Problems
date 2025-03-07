d6ba0f796d5150fe8f6abc73259baff56075f202
Sharvil21 <sshastri2102@gmail.com>
2025-03-07 12:32:16 -0500

    Added the solution

--- Solutions/Easy/884_Uncommon_Words_from_Two_Sentences.py
+++ Solutions/Easy/884_Uncommon_Words_from_Two_Sentences.py
@@ -1,1 +1,24 @@
 #First Solution: Create an empty dictionary, split the strings and get the count of each word in the dictionary. Then append only those words that have a value of 1 in an empty list
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        ans = {}
+        op = []
+        for i in s1.split():
+            if i in ans:
+                ans[i] += 1
+            else:
+                ans[i] = 1
+
+        for i in s2.split():
+            if i in ans:
+                ans[i] += 1
+            else:
+                ans[i] = 1
+
+        for word,count in ans.items():
+            if count == 1:
+                op.append(word)
+
+        return op
+
