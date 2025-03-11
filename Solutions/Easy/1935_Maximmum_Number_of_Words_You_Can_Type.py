ba9316e28c4153b23d0233928bcbf602f02f4626
Sharvil21 <sshastri2102@gmail.com>
2025-03-11 11:20:21 -0400

    Added the Python Solution

--- Solutions/Easy/1935_Maximmum_Number_of_Words_You_Can_Type.py
+++ Solutions/Easy/1935_Maximmum_Number_of_Words_You_Can_Type.py
@@ -1,1 +1,10 @@
 #Solution
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        count = len(text.split())
+        for word in text.split():
+            for j in brokenLetters:
+                if j in word:
+                    count -= 1
+                    break
+        return count
