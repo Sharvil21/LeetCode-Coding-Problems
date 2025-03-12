06e8fcb92d0db4ece2636b9a6fdbb4672ef8ae2f
Sharvil21 <sshastri2102@gmail.com>
2025-03-12 12:41:53 -0400

    Added the Python Solution

--- Solutions/Easy/2351_First_Letter_to_Appear_Twice_Python_Solution.py
+++ Solutions/Easy/2351_First_Letter_to_Appear_Twice_Python_Solution.py
@@ -1,1 +1,10 @@
 #Python Solution using set
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = set()
+        for letter in s:
+            if letter in seen:
+                return letter
+            else:
+                seen.add(letter)
+
