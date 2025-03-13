d8ddc4c9cecf2fc1d247bf3f852f11ef1a0d546b
Sharvil21 <sshastri2102@gmail.com>
2025-03-13 12:08:08 -0400

    Added the Python Solution

--- Solutions/Easy/2264_Largest_3-Same-Digit_Numer_in_String_Python_Solution.py
+++ Solutions/Easy/2264_Largest_3-Same-Digit_Numer_in_String_Python_Solution.py
@@ -1,1 +1,12 @@
-#Python Solution using for loog
+#Python Solution using for loop
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        nums = []
+        for i in range(len(num)-2):
+            if num[i] == num[i+1] and num[i] == num[i+2]:
+                added_num = num[i] + num[i+1] + num[i+2]
+                nums.append((added_num))
+        if len(nums) == 0:
+            return ""
+        else:
+            return str(max(nums))
