f9898755859c9592e68f7972a549fe44128dcd5a
Sharvil21 <sshastri2102@gmail.com>
2025-03-15 17:06:16 -0400

    Adding the Pandas Solution

--- Solutions/Easy/1661_Average_Time_of_Process_per_Machine_Pandas_Solution.py
+++ Solutions/Easy/1661_Average_Time_of_Process_per_Machine_Pandas_Solution.py
@@ -1,1 +1,1 @@
-#Pandas Solution using .diff() method. First sort the dataframe by machine_id, process_id then the timestamp. Then
+#Pandas Solution using .diff() method. First sort the dataframe by machine_id, process_id then the timestamp. Then group by machine_id and process_id, and use the .diff() method on the timestamp values to get the difference.
