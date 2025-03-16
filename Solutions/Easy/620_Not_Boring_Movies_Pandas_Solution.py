c033d34858d472a5db2e73dbbaa8965322514b13
Sharvil21 <sshastri2102@gmail.com>
2025-03-16 13:48:08 -0400

    Added another Pandas Solution

--- Solutions/Easy/620_Not_Boring_Movies_Pandas_Solution.py
+++ Solutions/Easy/620_Not_Boring_Movies_Pandas_Solution.py
@@ -6,4 +6,8 @@ import pandas as pd
 def not_boring_moveis(cinema: pd.DataFrame) -> pd.DataFrame:
 	return cinema.query("id%2 != 0 and description != 'boring'").sort_values(by="rating",ascending = False)
 
-#
+#Another Pandas Solution using str.contains() method
+
+def not_boring_moveis(cinema: pd.DataFrame) -> pd.DataFrame:
+	return cinema.query("id%2 != 0 and description.str.contains('boring')").sort_values(by="rating",ascending = False)
+
