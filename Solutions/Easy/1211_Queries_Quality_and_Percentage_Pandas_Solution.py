2f592441a559f52e7616757649b3f7e082eaf9b8
Sharvil21 <sshastri2102@gmail.com>
2025-03-17 14:16:46 -0400

    Adding the Pandas Solution

--- Solutions/Easy/1211_Queries_Quality_and_Percentage_Pandas_Solution.py
+++ Solutions/Easy/1211_Queries_Quality_and_Percentage_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #First , create a column called quality that takes the ratio of rating and position
+#For Leetcode Testcases, we have to add 1e-10 to the quality column as otherwise the rounding upto 2 digits doesn't work for the given testcases
