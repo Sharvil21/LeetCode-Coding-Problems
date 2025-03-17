7cb3e46a07e2af158ff9fc0348dd0e05d9d61cd6
Sharvil21 <sshastri2102@gmail.com>
2025-03-17 14:17:45 -0400

    Adding the Pandas Solution

--- Solutions/Easy/1211_Queries_Quality_and_Percentage_Pandas_Solution.py
+++ Solutions/Easy/1211_Queries_Quality_and_Percentage_Pandas_Solution.py
@@ -2,3 +2,4 @@
 #First , create a column called quality that takes the ratio of rating and position
 #For Leetcode Testcases, we have to add 1e-10 to the quality column as otherwise the rounding upto 2 digits doesn't work for the given testcases
 # Then create another column for poor query percentage by using .apply method and a lambda function
+#Lastly, group by the query_name, and use .agg method to aggregate the means for the two columns we want.
