ad95c204a1491d68e9c70690ee0352851f1caefd
Sharvil21 <sshastri2102@gmail.com>
2025-03-18 16:40:00 -0400

    Adding the Pandas Solution

--- Solutions/Medium/550_Game_Play_Analysis_IV_Pandas_Solution.py
+++ Solutions/Medium/550_Game_Play_Analysis_IV_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #First Sort the values by player_id and the event_date just for precaution
 #Then create a new column which shows the immediate next login date for each player. Can be done using .shift() method and group by each player_id
+#Then create another column which shows the difference between the next login date and the actual event date
