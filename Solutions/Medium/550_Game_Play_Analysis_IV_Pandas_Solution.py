5f3542c57cec8933d2f95527dddb314cb6e67f8b
Sharvil21 <sshastri2102@gmail.com>
2025-03-18 16:41:16 -0400

    Adding the Pandas Solution

--- Solutions/Medium/550_Game_Play_Analysis_IV_Pandas_Solution.py
+++ Solutions/Medium/550_Game_Play_Analysis_IV_Pandas_Solution.py
@@ -3,3 +3,4 @@
 #Then create a new column which shows the immediate next login date for each player. Can be done using .shift() method and group by each player_id
 #Then create another column which shows the difference between the next login date and the actual event date
 #Drop the duplicate values. There will be testcases where the difference will be 1 for same player id. So keep='first' in the drop_duplicates() method
+#Then count the number of rows in the resulting df, and then count unique players. Then get the ration
