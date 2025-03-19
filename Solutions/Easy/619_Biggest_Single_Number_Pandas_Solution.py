454941664a9f0197a8ee3a931d97e02279efaafe
Sharvil21 <sshastri2102@gmail.com>
2025-03-19 15:31:47 -0400

    Adding the Pandas Solution

--- Solutions/Easy/619_Biggest_Single_Number_Pandas_Solution.py
+++ Solutions/Easy/619_Biggest_Single_Number_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #Simple Approach:
-#Drop the duplicates first. This will remove all the numbers which don't appear once
+#Drop the duplicates first. This will remove all the numbers which don't appear once. Remember to have the argument keep = False.
+# Then
