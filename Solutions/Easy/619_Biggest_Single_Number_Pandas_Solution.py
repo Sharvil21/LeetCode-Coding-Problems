f84c2566868cd387c03694ab8343f02914b3cdea
Sharvil21 <sshastri2102@gmail.com>
2025-03-19 15:32:08 -0400

    Added the Pandas Solution

--- Solutions/Easy/619_Biggest_Single_Number_Pandas_Solution.py
+++ Solutions/Easy/619_Biggest_Single_Number_Pandas_Solution.py
@@ -1,4 +1,8 @@
 #Pandas Solution
 #Simple Approach:
 #Drop the duplicates first. This will remove all the numbers which don't appear once. Remember to have the argument keep = False.
-# Then
+# Then get the max value of the resulting dataset and return it
+import pandas as pd
+
+def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
+    return my_numbers.drop_duplicates(keep=False).max().to_frame('num')
