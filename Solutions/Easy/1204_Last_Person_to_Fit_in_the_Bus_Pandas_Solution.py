88e0afe3772e58246f1c975551c11561f5810982
Sharvil21 <sshastri2102@gmail.com>
2025-03-20 15:44:39 -0400

    Adding the Pandas Solution

--- Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_Pandas_Solution.py
+++ Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #Create a column which gives the cumulative sum/rolling sum of weight
 #We can use the .cumsum() method for this
+#Then only get the last row where the cumulative sum <=1000
