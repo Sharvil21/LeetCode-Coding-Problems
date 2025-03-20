b6fcf4072731691f7741041d93da4c113ff1f040
Sharvil21 <sshastri2102@gmail.com>
2025-03-20 15:44:50 -0400

    Added the Pandas Solution

--- Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_Pandas_Solution.py
+++ Solutions/Easy/1204_Last_Person_to_Fit_in_the_Bus_Pandas_Solution.py
@@ -2,3 +2,9 @@
 #Create a column which gives the cumulative sum/rolling sum of weight
 #We can use the .cumsum() method for this
 #Then only get the last row where the cumulative sum <=1000
+import pandas as pd
+
+def last_passenger(queue: pd.DataFrame) -> pd.DataFrame:
+    queue.sort_values(by='turn',inplace=True)
+    queue['weight_till_this_turn'] = queue['weight'].cumsum()
+    return queue.loc[queue['weight_till_this_turn']<=1000,'person_name'].tail(1).to_frame('person_name')
