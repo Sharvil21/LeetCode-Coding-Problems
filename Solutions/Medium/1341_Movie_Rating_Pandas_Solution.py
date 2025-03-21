ef7fb87e3d4c55ec71a9de872024ad8d3885b820
Sharvil21 <sshastri2102@gmail.com>
2025-03-21 16:59:34 -0400

    Adding the Pandas Solution

--- Solutions/Medium/1341_Movie_Rating_Pandas_Solution.py
+++ Solutions/Medium/1341_Movie_Rating_Pandas_Solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 #For first output,merge the users table and movie_rating table on user_id
 #Then simply group by the 'name', and find the count of movie_id, reset the index, then sort by the total ratings Descending and the name ascending (so that they are sorted lexicographically)
+#Then store the value of the highest movie rater inside a variable using .iloc method
