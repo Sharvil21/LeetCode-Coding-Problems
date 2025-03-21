26e9442fb8110ccc48e57db90deafb2b966ac614
Sharvil21 <sshastri2102@gmail.com>
2025-03-21 17:00:13 -0400

    Adding the Pandas Solution

--- Solutions/Medium/1341_Movie_Rating_Pandas_Solution.py
+++ Solutions/Medium/1341_Movie_Rating_Pandas_Solution.py
@@ -3,3 +3,4 @@
 #Then simply group by the 'name', and find the count of movie_id, reset the index, then sort by the total ratings Descending and the name ascending (so that they are sorted lexicographically)
 #Then store the value of the highest movie rater inside a variable using .iloc method
 #For second output, merge the movie_rating table with movies table
+#Then, use .loc to filter out those rows where the crreated at date is only in feb 2020
