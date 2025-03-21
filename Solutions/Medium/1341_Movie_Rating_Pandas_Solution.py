8eadea0e47610d31f276d822832b29a9c31e0745
Sharvil21 <sshastri2102@gmail.com>
2025-03-21 16:58:58 -0400

    Created the .py file for Pandas Solution

--- Solutions/Medium/1341_Movie_Rating_Pandas_Solution.py
+++ Solutions/Medium/1341_Movie_Rating_Pandas_Solution.py
@@ -1,3 +1,3 @@
 #Pandas Solution
 #For first output,merge the users table and movie_rating table on user_id
-#Then simply group by the 'name', and find the count of movie_id, reset the index
+#Then simply group by the 'name', and find the count of movie_id, reset the index, then sort by the total ratings Descending and the name ascending (so that they are sorted lexicographically)
