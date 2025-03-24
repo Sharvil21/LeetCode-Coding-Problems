2fb3c7b0064aac0d2801948b4b25c1f9e14dfa3e
Sharvil21 <sshastri2102@gmail.com>
2025-03-24 15:47:50 -0400

    Adding the Pandas Solution

--- Solutions/Easy/1327_List_the_Products_Ordered_in_a_Period_Pandas_Solution.py
+++ Solutions/Easy/1327_List_the_Products_Ordered_in_a_Period_Pandas_Solution.py
@@ -2,4 +2,5 @@
 #First, merge the two tables
 #Then, I preferred to create a new column tat shows the Month and Year only for the order date of that row
 #Then filter out only those rows which are in Feb 2020
+#All that's left is to group by, then take the sum
 import pandas as pd
