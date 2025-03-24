78efe9ba144e7e9d7323b88a5956ce873e58ea9f
Sharvil21 <sshastri2102@gmail.com>
2025-03-24 15:46:14 -0400

    Adding the Pandas Solution

--- Solutions/Easy/1327_List_the_Products_Ordered_in_a_Period_Pandas_Solution.py
+++ Solutions/Easy/1327_List_the_Products_Ordered_in_a_Period_Pandas_Solution.py
@@ -1,4 +1,5 @@
 #Pandas Solution
 #First, merge the two tables
 #Then, I preferred to create a new column tat shows the Month and Year only for the order date of that row
+#Then filter out only those rows which are in Feb 2020
 import pandas as pd
