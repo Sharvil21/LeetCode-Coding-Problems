3e7c71a81eaac6dfefcacab0dcc61c74972a8e73
Sharvil21 <sshastri2102@gmail.com>
2025-03-24 15:48:26 -0400

    Added the Pandas Solution

--- Solutions/Easy/1327_List_the_Products_Ordered_in_a_Period_Pandas_Solution.py
+++ Solutions/Easy/1327_List_the_Products_Ordered_in_a_Period_Pandas_Solution.py
@@ -3,3 +3,10 @@
 #Then, I preferred to create a new column tat shows the Month and Year only for the order date of that row
 #Then filter out only those rows which are in Feb 2020
 #All that's left is to group by, then take the sum and use the .loc[] to return the required columns
+import pandas as pd
+
+def list_products(products: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = products.merge(orders,on='product_id',how='inner')
+    df['month_year'] = df['order_date'].dt.strftime("%Y-%m")
+    df2 = df.loc[df['month_year'] == "2020-02"].groupby(['product_id','product_name'],as_index=False).agg(unit=('unit','sum'))
+    return df2.loc[df2['unit']>=100,['product_name','unit']]
