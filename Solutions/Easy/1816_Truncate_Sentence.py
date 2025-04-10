f09bcf18f7e4880c72493a6fa1d10627f7cfa742
Sharvil21 <sshastri2102@gmail.com>
2025-04-09 22:38:05 -0400

    Attempting the problem again

--- Solutions/Easy/1816_Truncate_Sentence.py
+++ Solutions/Easy/1816_Truncate_Sentence.py
@@ -5,3 +5,6 @@ class Solution:
 
 
 #Attempting again. Came up with the same solution
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        return " ".join(s.split()[:k])
