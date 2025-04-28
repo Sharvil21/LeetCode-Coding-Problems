916ce0276330b0e71ee1e345825d8d3081235aed
Sharvil21 <sshastri2102@gmail.com>
2025-04-28 13:44:31 -0400

    Adding the Python Solution

--- Solutions/Easy/728_Self_Dividing_Numbers_Python_Solution.py
+++ Solutions/Easy/728_Self_Dividing_Numbers_Python_Solution.py
@@ -2,3 +2,4 @@
 #Have to use for loops and iterations to check the condition
 #Create an empty list first
 #Then iterate a for loop in the range specified
+#Then for each iteration, convert it to a string, and run a for loop for each value in the string.
