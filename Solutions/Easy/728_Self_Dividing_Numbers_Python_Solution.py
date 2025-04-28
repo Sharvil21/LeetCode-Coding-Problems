2b9a0a0408d617100d703cb04452baf0a771d023
Sharvil21 <sshastri2102@gmail.com>
2025-04-28 13:47:14 -0400

    Added the Python Solution

--- Solutions/Easy/728_Self_Dividing_Numbers_Python_Solution.py
+++ Solutions/Easy/728_Self_Dividing_Numbers_Python_Solution.py
@@ -5,4 +5,15 @@
 #Then for each iteration, convert it to a string, and run a for loop for each value in the string.
 #If the value is 0 or the modulus isn't 0, then break the loop.
 #else, append the value into the list
-
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        op = []
+        for num in range(left,right+1):
+            valid = True
+            for digit in str(num):
+                if int(digit) == 0 or num%int(digit) != 0 :
+                    valid = False
+                    break
+            if valid:
+                op.append(num)
+        return op
