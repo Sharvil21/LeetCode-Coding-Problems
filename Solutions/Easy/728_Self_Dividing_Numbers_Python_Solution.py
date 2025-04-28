db6d560093ca3115c7d12b75ccf0257b0373fc56
Sharvil21 <sshastri2102@gmail.com>
2025-04-28 13:45:21 -0400

    Adding the Python Solution

--- Solutions/Easy/728_Self_Dividing_Numbers_Python_Solution.py
+++ Solutions/Easy/728_Self_Dividing_Numbers_Python_Solution.py
@@ -3,3 +3,6 @@
 #Create an empty list first
 #Then iterate a for loop in the range specified
 #Then for each iteration, convert it to a string, and run a for loop for each value in the string.
+#If the value is 0 or the modulus isn't 0, then break the loop.
+#else, append the value into the list
+
