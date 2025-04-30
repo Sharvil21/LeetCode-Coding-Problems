37ab9e8899d007823cb33d10863c4adb67532f22
Sharvil21 <sshastri2102@gmail.com>
2025-04-29 20:30:43 -0400

    Adding the Python Solution

--- Solutions/Easy/1748_Sum_of_Unique_Elements_Python_Solution.py
+++ Solutions/Easy/1748_Sum_of_Unique_Elements_Python_Solution.py
@@ -1,3 +1,5 @@
 #Python Solution
 #Used Counter from collections library
 #Create a counter of the numbers in the list
+#Iterate through it, then add only the unique elements in a different list
+#Lastly, return the sum of that new list
