351bccf84520ab77e9187bf97d5f746d3b95f905
Sharvil21 <sshastri2102@gmail.com>
2025-04-29 20:31:01 -0400

    Added the Python Solution

--- Solutions/Easy/1748_Sum_of_Unique_Elements_Python_Solution.py
+++ Solutions/Easy/1748_Sum_of_Unique_Elements_Python_Solution.py
@@ -3,3 +3,12 @@
 #Create a counter of the numbers in the list
 #Iterate through it, then add only the unique elements in a different list
 #Lastly, return the sum of that new list
+from collections import Counter
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        unique_elements = []
+        for num, total in count.items():
+            if total == 1:
+                unique_elements.append(num)
+        return sum(unique_elements)
