08f197c8ba13ca19c7bc0a534b2a4218148bbb48
Sharvil21 <sshastri2102@gmail.com>
2025-04-30 20:01:16 -0400

    Added the Python Solution (2nd Attempt)

--- Solutions/Easy/3110_Score_of_a_String_Python_Solution.py
+++ Solutions/Easy/3110_Score_of_a_String_Python_Solution.py
@@ -1,1 +1,8 @@
 #Python Solution (2nd Attempt)
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(len(s)-1):
+            score += abs(ord(s[i]) - ord(s[i+1]))
+        return score
+
