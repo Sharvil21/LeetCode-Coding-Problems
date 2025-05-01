c4da8d71c051debd2593397322d85e517f906b33
Sharvil21 <sshastri2102@gmail.com>
2025-04-30 20:04:34 -0400

    Added the Pandas Solution (2nd attempt)

--- Solutions/Easy/2881_Create_a_New_Column_Pandas_Solution.py
+++ Solutions/Easy/2881_Create_a_New_Column_Pandas_Solution.py
@@ -1,2 +1,7 @@
 #Pandas Solution
 #Just create the new column simply and return the dataframe
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary']*2
+    return employees
