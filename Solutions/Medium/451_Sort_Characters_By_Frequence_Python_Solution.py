e539077cd214db2936c772aa0f83eb0d1b2844ae
Sharvil21 <sshastri2102@gmail.com>
2025-05-05 19:14:45 -0400

    Adding the Python Solution

--- Solutions/Medium/451_Sort_Characters_By_Frequence_Python_Solution.py
+++ Solutions/Medium/451_Sort_Characters_By_Frequence_Python_Solution.py
@@ -1,3 +1,4 @@
 #Python Solution
 #Will have to use the Counter library from collections
 #Create a dictionary first using Counter.
+#The logic is to sort the Counter dictionary using the frequency of the keys in descending order. We can use the sorted() function for this. But We'll also have to use a lambda function
