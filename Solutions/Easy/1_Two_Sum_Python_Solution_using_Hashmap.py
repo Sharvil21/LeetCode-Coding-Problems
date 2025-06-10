43f5b861a25f7ebc53b30740a34ce5de30d78b14
Sharvil21 <sshastri2102@gmail.com>
2025-06-10 10:30:31 -0400

    Adding the Python Solution

--- Solutions/Easy/1_Two_Sum_Python_Solution_using_Hashmap.py
+++ Solutions/Easy/1_Two_Sum_Python_Solution_using_Hashmap.py
@@ -1,2 +1,4 @@
 #Python Solution using Hashmap
-#Create a hashmap.
+#Create a hashmap first.
+#Then iterate through the list. Create a 'difference' variable that is difference = target - value_in_list
+#If difference exists in the hashmap, return that index
