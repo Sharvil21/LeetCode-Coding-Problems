f17c83340c85b25f7a81942eb5522458db52fa69
Sharvil21 <sshastri2102@gmail.com>
2025-06-10 10:30:50 -0400

    Adding the Python Solution

--- Solutions/Easy/1_Two_Sum_Python_Solution_using_Hashmap.py
+++ Solutions/Easy/1_Two_Sum_Python_Solution_using_Hashmap.py
@@ -2,3 +2,13 @@
 #Create a hashmap first.
 #Then iterate through the list. Create a 'difference' variable that is difference = target - value_in_list
 #If difference exists in the hashmap, return that index
+#Else, add that value along with the index into the hashmap
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for index, value in enumerate(nums):
+            difference = target - value
+            if difference in hashmap:
+                return [index, hashmap[difference]]
+            else:
+                hashmap[value] = index
